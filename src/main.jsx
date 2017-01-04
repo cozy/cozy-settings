f52baf19811a0842fbe0a7811551221c52a4188f
Cedric Patchane Lacane <cedric@cozycloud.cc>
2017-01-04 15:33:19 +0100

    [feat] Add main app navigation using router

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import './styles/main'
 
 import React from 'react'
 import { render } from 'react-dom'
+import { Router, Route, Redirect, hashHistory } from 'react-router'
 import { I18n } from './plugins/preact-polyglot'
 
 import App from './components/App'
@@ -14,7 +15,19 @@ const lang = document.documentElement.getAttribute('lang') || 'en'
 document.addEventListener('DOMContentLoaded', () => {
   render((
     <I18n context={context} lang={lang}>
-      <App />
+      <Router history={hashHistory}>
+        <Route component={(props) =>
+          <App {...props} />}
+        >
+          <Redirect from='/' to='account' />
+          <Route
+            path='account'
+            component={(props) =>
+              <p>Hello World</p>
+            }
+          />
+        </Route>
+      </Router>
     </I18n>
   ), document.querySelector('[role=application]'))
 })
