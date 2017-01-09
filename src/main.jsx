9a9e276aec28995b29605dc83bbf711960cc907f
Cedric Patchane Lacane <cedric@cozycloud.cc>
2017-01-09 17:40:27 +0100

    [feat] Add account store

--- src/main.jsx
+++ src/main.jsx
@@ -7,45 +7,54 @@ import { render } from 'react-dom'
 import { Router, Route, Redirect, hashHistory } from 'react-router'
 import { I18n } from './plugins/preact-polyglot'
 
+import AccountStore from './store/AccountStore'
+import Provider from './store/Provider'
+
 import App from './components/App'
+import AccountManagement from './containers/AccountManagement'
 
 const context = window.context
 const lang = document.documentElement.getAttribute('lang') || 'en'
 
+// store
+const store = new AccountStore()
+
 document.addEventListener('DOMContentLoaded', () => {
   render((
-    <I18n context={context} lang={lang}>
-      <Router history={hashHistory}>
-        <Route component={(props) =>
-          <App {...props} />}
-        >
-          <Redirect from='/' to='account' />
-          <Route
-            path='account'
-            component={(props) =>
-              <p>Account</p>
-            }
-          />
-          <Route
-            path='connectedDevices'
-            component={(props) =>
-              <p>Connected devices</p>
-            }
-          />
-          <Route
-            path='storage'
-            component={(props) =>
-              <p>Storage</p>
-            }
-          />
-          <Route
-            path='emailNotifications'
-            component={(props) =>
-              <p>Email Notifications</p>
-            }
-          />
-        </Route>
-      </Router>
-    </I18n>
+    <Provider store={store}>
+      <I18n context={context} lang={lang}>
+        <Router history={hashHistory}>
+          <Route component={(props) =>
+            <App {...props} />}
+          >
+            <Redirect from='/' to='account' />
+            <Route
+              path='account'
+              component={(props) =>
+                <AccountManagement {...props} />
+              }
+            />
+            <Route
+              path='connectedDevices'
+              component={(props) =>
+                <h2>Connected devices</h2>
+              }
+            />
+            <Route
+              path='storage'
+              component={(props) =>
+                <h2>Storage</h2>
+              }
+            />
+            <Route
+              path='emailNotifications'
+              component={(props) =>
+                <h2>Email Notifications</h2>
+              }
+            />
+          </Route>
+        </Router>
+      </I18n>
+    </Provider>
   ), document.querySelector('[role=application]'))
 })
