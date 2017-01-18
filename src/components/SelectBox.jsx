77d3eadf7044aaac46ab9947e368154be04f1fae
GoOz <gooz@foojin.com>
2017-01-18 12:25:56 +0100

    [Feat] Add locales to SelectBox component

--- src/components/SelectBox.jsx
+++ src/components/SelectBox.jsx
@@ -1,15 +1,29 @@
 import styles from '../styles/passphraseForm'
 
 import React from 'react'
+import ReactMarkdown from 'react-markdown'
 
 const SelectBox = ({t, InputData}) => (
   <div className={styles['coz-form']}>
     <h3>{t(`AccountView.${InputData}.title`)}</h3>
-    <label className={styles['coz-desc']}>{t(`AccountView.${InputData}.label`)}</label>
+    <label className={styles['coz-desc']}>
+      {t(`AccountView.${InputData}.label`)}
+    </label>
     <select>
-      <option value='en'>English</option>
-      <option value='fr'>Français</option>
+      <option value={t(`AccountView.${InputData}.english.value`)}>
+        {t(`AccountView.${InputData}.english.text`)}
+      </option>
+      <option value={t(`AccountView.${InputData}.french.value`)}>
+        {t(`AccountView.${InputData}.french.text`)}
+      </option>
     </select>
+    <p className={styles['coz-desc']}>
+      <ReactMarkdown source={
+        t(`AccountView.${InputData}.contrib`, {
+          url: 'http://cozy.io'
+        })
+      } />
+    </p>
   </div>
 )
 
