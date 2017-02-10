043ad3b2b589e27b8ec1ad8056fce3c096660a64
Grégory Le Garec <gregory.legarec@gmail.com>
2017-02-10 18:58:23 +0100

    [feat] Switch locale and password position in form

--- src/components/AccountView.jsx
+++ src/components/AccountView.jsx
@@ -15,7 +15,6 @@ const AccountView = ({ t, fields, passphrase, isFetching, onFieldChange, onPassp
     <h2>{t('AccountView.title')}</h2>
     <Input name='email' type='email' {...fields.email} onChange={onFieldChange} />
     <Input name='public_name' type='text' {...fields.public_name} onChange={onFieldChange} />
-    <PassphraseForm {...passphrase} onSubmit={onPassphraseSubmit} />
     <Select name='locale' options={LANG_OPTIONS} {...fields.locale} onChange={onFieldChange} />
     <p className={styles['account-view-desc']}>
       <ReactMarkdown
@@ -25,6 +24,7 @@ const AccountView = ({ t, fields, passphrase, isFetching, onFieldChange, onPassp
         renderers={{Link: props => <a href={props.href} target='_blank'>{props.children}</a>}}
       />
     </p>
+    <PassphraseForm {...passphrase} onSubmit={onPassphraseSubmit} />
   </div>
 )
 
