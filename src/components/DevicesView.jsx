3d80abef9150ce19c6d4ce8206654b65b16d93c4
Yannick Lohse <contact@yannick-lohse.fr>
2017-02-23 11:39:39 +0100

    [feat] Added empty view

--- src/components/DevicesView.jsx
+++ src/components/DevicesView.jsx
@@ -6,39 +6,46 @@ import React from 'react'
 import { translate } from 'cozy-ui/react/helpers/i18n'
 
 import Loading from './Loading'
+import Empty from './Empty'
+
+//for the icon, we show a phone if it's a phone, and a laptop in all other cases
+const getDeviceKindClass = kind => kind === 'mobile' ? styles['set-device-phone'] : styles['set-device-laptop']
 
 const DevicesView = ({ t, f, isFetching, devices }) => (
   <div className={devicesStyles['devices-view']}>
     <h2>{t('DevicesView.title')}</h2>
     { isFetching && <Loading /> }
-    <div className={classNames(
-      styles['set-content-table']
-    )}>
-      <div className={styles['set-content-row']}>
-        <div className={classNames(styles['set-content-header'], styles['set-content-primary'])}>{ t('DevicesView.head_name') }</div>
-        <div className={classNames(styles['set-content-header'], styles['set-content-secondary'])}>{ t('DevicesView.head_activity') }</div>
-        <div className={classNames(styles['set-content-header'], styles['set-content-secondary'])}>{ t('DevicesView.head_permissions') }</div>
-        <div className={classNames(styles['set-content-header'])}>{ t('DevicesView.head_actions') }</div>
-      </div>
-      {devices.map(device => (
-      <div className={styles['set-content-row']}>
-        <div className={classNames(styles['set-content-cell'], styles['set-content-primary'], device.client_kind === 'mobile' ? styles['set-device-phone'] : styles['set-device-laptop'])}>
-          {device.client_name}
+    { !isFetching && devices.length === 0 && <Empty emptyType='devices' />}
+    { devices.length > 0 && (
+      <div className={classNames(
+        styles['set-content-table']
+      )}>
+        <div className={styles['set-content-row']}>
+          <div className={classNames(styles['set-content-header'], styles['set-content-primary'])}>{ t('DevicesView.head_name') }</div>
+          <div className={classNames(styles['set-content-header'], styles['set-content-secondary'])}>{ t('DevicesView.head_activity') }</div>
+          <div className={classNames(styles['set-content-header'], styles['set-content-secondary'])}>{ t('DevicesView.head_permissions') }</div>
+          <div className={classNames(styles['set-content-header'])}>{ t('DevicesView.head_actions') }</div>
         </div>
-        <div className={classNames(styles['set-content-cell'], styles['set-content-secondary'])}>
+        {devices.map(device => (
+        <div className={styles['set-content-row']}>
+          <div className={classNames(styles['set-content-cell'], styles['set-content-primary'], getDeviceKindClass(device.client_kind))}>
+            {device.client_name}
+          </div>
+          <div className={classNames(styles['set-content-cell'], styles['set-content-secondary'])}>
 
-        </div>
-        <div className={classNames(styles['set-content-cell'], styles['set-content-secondary'])}>
+          </div>
+          <div className={classNames(styles['set-content-cell'], styles['set-content-secondary'])}>
 
+          </div>
+          <div className={classNames(styles['set-content-cell'])}>
+            <button className={classNames(devicesStyles['coz-btn--revoke'])}>
+              {t('DevicesView.revoke')}
+            </button>
+          </div>
         </div>
-        <div className={classNames(styles['set-content-cell'])}>
-          <button className={classNames(devicesStyles['coz-btn--revoke'])}>
-            {t('DevicesView.revoke')}
-          </button>
-        </div>
+        ))}
       </div>
-      ))}
-    </div>
+    )}
   </div>
 )
 
