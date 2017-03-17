da3be2cd9f94bb9bd6b4e25d61eeff46b5330824
GoOz <gooz@foojin.com>
2017-03-17 18:48:00 +0100

    [feat] Add table style — WIP

--- src/components/ServicesView.jsx
+++ src/components/ServicesView.jsx
@@ -1,6 +1,6 @@
-// import tableStyles from '../styles/table'
+import tableStyles from '../styles/table'
 import viewStyles from '../styles/view.styl'
-// import classNames from 'classnames'
+import classNames from 'classnames'
 
 import React from 'react'
 import { translate } from 'cozy-ui/react/helpers/i18n'
@@ -10,6 +10,14 @@ import { translate } from 'cozy-ui/react/helpers/i18n'
 const DevicesView = ({ t, f, devices }) => (
   <div>
     <h2 className={viewStyles['set-view-title']}>{t('ServicesView.title')}</h2>
+    <div className={classNames(tableStyles['coz-table'])}>
+      <div className={classNames(tableStyles['coz-table-head'], tableStyles['coz-table-row'])}>
+        <div className={classNames(tableStyles['coz-table-header'], tableStyles['coz-table-service'])}>{ t('DevicesView.head_services') }</div>
+        <div className={classNames(tableStyles['coz-table-header'], tableStyles['coz-table-account'])}>{ t('DevicesView.head_account') }</div>
+        <div className={classNames(tableStyles['coz-table-header'], tableStyles['coz-table-sync'])}>{ t('DevicesView.head_sync') }</div>
+        <div className={classNames(tableStyles['coz-table-header'], tableStyles['coz-table-status'])}>{ t('DevicesView.head_status') }</div>
+      </div>
+    </div>
   </div>
 )
 
