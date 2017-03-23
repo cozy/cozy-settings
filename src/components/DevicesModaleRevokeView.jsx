7c20250bb5cc63ea5ff302c3fb73558db74bcdff
Olivier Gendrin — Faire Front <olivier@fairefront.fr>
2017-03-23 17:14:55 +0100

    feat: revoke device, the CSS part

--- src/components/DevicesModaleRevokeView.jsx
+++ src/components/DevicesModaleRevokeView.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
 import { translate } from 'cozy-ui/react/helpers/i18n'
+
 import Modal from 'cozy-ui/react/Modal'
 
+import ReactMarkdown from 'react-markdown'
+
+import classNames from 'classnames'
+import styles from '../styles/devicesModaleRevokeView'
+
 const devicesModaleRevokeView = ({ t, device, revokeDevice, cancelAction }) => {
+  const descriptionText = `${t('revokeDevice.description', {name: device.client_name})}\n\n${t('revokeDevice.subText')}`
+
   return (<Modal
     title={t('revokeDevice.title')}
-    description={t('revokeDevice.description', {name: device.client_name})}
-    subText={t('revokeDevice.subText')}
-    cancelText='cancel'
-    cancelAction={() => cancelAction()}
     validateText={t('revokeDevice.validate')}
     validateType='danger'
     validateAction={() => revokeDevice(device.id)}
-  />)
+    cancelText='cancel'
+    cancelType='secondary'
+    cancelAction={() => cancelAction()}
+    >
+    <div className={classNames(styles['coz-modal-content'], styles['coz-modal-revoke-content'])}>
+      <ReactMarkdown source={descriptionText} />
+    </div>
+  </Modal>)
 }
 
 export default translate()(devicesModaleRevokeView)
