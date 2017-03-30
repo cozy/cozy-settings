8bd56ced569fd22df3d15c5613cf1fa91fa2f32f
Grégory Le Garec <gregory.legarec@gmail.com>
2017-03-30 09:35:06 +0200

    [refacto] Clean unused name parameter in Field :pencil:

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -5,7 +5,7 @@ import React from 'react'
 import classNames from 'classnames'
 import { translate } from 'cozy-ui/react/helpers/i18n'
 
-const Field = ({ t, name, label, description, submitting, saved, errors, children }) => (
+const Field = ({ t, label, description, submitting, saved, errors, children }) => (
   <div className={classNames(styles['coz-form'], styles['set-field'], {
     [styles['set-field-loading']]: submitting,
     [styles['set-field-saved']]: saved
