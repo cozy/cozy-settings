2e1d2ddea00fe88d3719297392266c1eed8dba72
CPatchane <code@patchane.com>
2017-06-13 11:48:48 +0200

    style: better set-field styles code :art:

--- src/components/Field.jsx
+++ src/components/Field.jsx
@@ -10,7 +10,8 @@ const Field = ({ t, type, label, description, submitting, saved, errors, childre
   <div className={classNames(styles['coz-form'], styles['set-field'], {
     [styles['set-field-loading']]: submitting,
     [styles['set-field-saved']]: saved,
-    [styles['set-field--checkbox']]: type === 'checkbox'
+    [styles['set-field-input']]: type !== 'checkbox',
+    [styles['set-field-checkbox']]: type === 'checkbox'
   })}>
     <h3 className={classNames(viewStyles['set-view-subtitle'], styles['set-field-title'])}>{label}</h3>
     <label className={styles['coz-form-desc']}>
