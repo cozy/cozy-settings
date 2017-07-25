75192fc5a85e8a77b0d5b2a7c5debe017523404f
CPatchane <code@patchane.com>
2017-07-25 16:27:49 +0200

    feat: handle claudy extra infos fetching in redux parts :sparkles:

--- src/reducers/claudy.js
+++ src/reducers/claudy.js
@@ -3,7 +3,8 @@ import { combineReducers } from 'redux'
 import {
   FETCH_CLAUDY_INFOS,
   FETCH_CLAUDY_INFOS_SUCCESS,
-  FETCH_CLAUDY_INFOS_FAILURE
+  FETCH_CLAUDY_INFOS_FAILURE,
+  FETCH_DEVICES_SUCCESS
 } from '../actions'
 
 export const actions = (state = [], action) => {
@@ -19,6 +20,15 @@ export const apps = (state = [], action) => {
   switch (action.type) {
     case FETCH_CLAUDY_INFOS_SUCCESS:
       return action.apps
+    default:
+      return state
+  }
+}
+
+export const devices = (state = [], action) => {
+  switch (action.type) {
+    case FETCH_DEVICES_SUCCESS:
+      return action.devices
     default:
       return state
   }
@@ -45,6 +55,7 @@ export const isFetching = (state = false, action) => {
 const claudy = combineReducers({
   actions,
   apps,
+  devices,
   error,
   isFetching
 })
