9da66cf8bee06bf2ad17063ed540284fa35562ab
Marius <marius@cozycloud.cc>
2018-01-30 10:44:30 +0100

    feat: desactivate 2FA

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -1,7 +1,7 @@
 import viewStyles from '../styles/view'
 
 import classNames from 'classnames'
-import React, { Component } from 'react'
+import React, { Component, Fragment } from 'react'
 
 import { Button } from 'cozy-ui/react/Button'
 import { translate } from 'cozy-ui/react/I18n'
@@ -33,6 +33,11 @@ class ProfileView extends Component {
   activate2FA () {
     // TODO: Open the password modal
     this.props.updateInfo('twofa', true)
+    this.closeTwoFAModal()
+  }
+  desactivate2FA () {
+    // TODO: Open the password modal
+    this.props.updateInfo('twofa', false)
     this.closeTwoFAModal()
   }
   openTwoFAModal () {
@@ -67,7 +72,7 @@ class ProfileView extends Component {
           <Input
             name='twofa'
             type='checkbox'
-            label={t('ProfileView.twofa.title')}
+            label={t('ProfileView.twofa.title.activate')}
             description={t('ProfileView.twofa.label', {link: 'https://support.cozy.io/article/114-doubleauthentification'})}
             {...fields.twofa}
             onChange={this.openTwoFAModal}
@@ -102,56 +107,99 @@ class ProfileView extends Component {
           <a href={t('ProfileView.tos.version', {version: instance && instance.data.attributes.tos ? `-${instance.data.attributes.tos}` : '-201711'})} target='_blank'>
             {t('ProfileView.tos.link')}
           </a>
-        </div>
-        {
-          twoFAModalIsOpen && <Modal
-            dismissAction={() => this.closeTwoFAModal()}
-            title={t('ProfileView.twofa.title')}
-          >
-            <ModalContent
-              className={viewStyles['set-view-content-twofa-modal']}
-            >
-              <img
-                alt={t('ProfileView.twofa.title')}
-                src={twoFaModalBanner}
-              />
-              <h3>{t('ProfileView.twofa.modal.protect')}</h3>
-              <p>
-                <ReactMarkdownWrapper
-                  source={
-                    t('ProfileView.twofa.modal.change', {link: 'https://support.cozy.io/article/114-doubleauthentification'})
+          {
+            twoFAModalIsOpen && <div className={viewStyles['set-view-content-twofa-modal-wrapper']}>
+              <Modal
+                dismissAction={() => this.closeTwoFAModal()}
+                className={viewStyles['set-view-content-twofa-modal']}
+                title={
+                  t(fields.twofa.value
+                    ? 'ProfileView.twofa.title.desactivate'
+                    : 'ProfileView.twofa.title.activate'
+                  )
+                }
+              >
+                <ModalContent
+                  className={viewStyles['set-view-content-twofa-modal-content']}
+                >
+                  {
+                    fields.twofa.value
+                    ? <Desactivate2FA
+                      desactivate2FA={() => this.desactivate2FA()}
+                      closeTwoFAModal={() => this.closeTwoFAModal()}
+                    />
+                    : <Activate2FA
+                      activate2FA={() => this.activate2FA()}
+                    />
                   }
-                />
-              </p>
-              <div className={viewStyles['set-view-content-twofa']}>
-                <div className={viewStyles['set-view-content-twofa-point']}>
-                  <img className={viewStyles['set-view-content-twofa-point-image']} alt="{t('ProfileView.twofa.modal.secu_title')}" src={twoFaModalSecu} />
-                  <div>
-                    <b>{t('ProfileView.twofa.modal.secu_title')}</b>
-                    <p>{t('ProfileView.twofa.modal.secu_description')}</p>
-                  </div>
-                </div>
-                <div className={viewStyles['set-view-content-twofa-point']}>
-                  <img className={viewStyles['set-view-content-twofa-point-image']} alt="{t('ProfileView.twofa.modal.protect_title')}" src={twoFaModalProtect} />
-                  <div>
-                    <b>{t('ProfileView.twofa.modal.protect_title')}</b>
-                    <p>{t('ProfileView.twofa.modal.protect_description')}</p>
-                  </div>
-                </div>
-              </div>
-              <div className={viewStyles['set-view-content-twofa-modal-button']}>
-                <Button
-                  onClick={() => this.activate2FA()}>
-                  {t('ProfileView.twofa.modal.button.activate')}
-                </Button>
-              </div>
-            </ModalContent>
-          </Modal>
-        }
-
+                </ModalContent>
+              </Modal>
+            </div>
+          }
+        </div>
       </div>
     )
   }
 }
+
+const Activate2FA = translate()(({t, activate2FA}) => (
+  <Fragment>
+    <img
+      alt={t('ProfileView.twofa.title')}
+      src={twoFaModalBanner}
+    />
+    <h3>{t('ProfileView.twofa.modal.protect')}</h3>
+    <p>
+      <ReactMarkdownWrapper
+        source={
+          t('ProfileView.twofa.modal.change', {link: 'https://support.cozy.io/article/114-doubleauthentification'})
+        }
+      />
+    </p>
+    <div className={viewStyles['set-view-content-twofa']}>
+      <div className={viewStyles['set-view-content-twofa-point']}>
+        <img className={viewStyles['set-view-content-twofa-point-image']} alt="{t('ProfileView.twofa.modal.secu_title')}" src={twoFaModalSecu} />
+        <div>
+          <b>{t('ProfileView.twofa.modal.secu_title')}</b>
+          <p>{t('ProfileView.twofa.modal.secu_description')}</p>
+        </div>
+      </div>
+      <div className={viewStyles['set-view-content-twofa-point']}>
+        <img className={viewStyles['set-view-content-twofa-point-image']} alt="{t('ProfileView.twofa.modal.protect_title')}" src={twoFaModalProtect} />
+        <div>
+          <b>{t('ProfileView.twofa.modal.protect_title')}</b>
+          <p>{t('ProfileView.twofa.modal.protect_description')}</p>
+        </div>
+      </div>
+    </div>
+    <div className={viewStyles['set-view-content-twofa-modal-content-button']}>
+      <Button
+        onClick={activate2FA}>
+        {t('ProfileView.twofa.modal.button.activate')}
+      </Button>
+    </div>
+  </Fragment>
+))
+
+const Desactivate2FA = translate()(({t, desactivate2FA, closeTwoFAModal}) => (
+  <Fragment>
+    <p><b>{t('ProfileView.twofa.modal.desactivate_title')}</b></p>
+    <p>{t('ProfileView.twofa.modal.desactivate_description')}</p>
+    <div className={viewStyles['set-view-content-twofa-modal-content-desactivate-buttons']}>
+      <Button
+        onClick={closeTwoFAModal}
+        theme='secondary'
+        >
+        {t('ProfileView.twofa.modal.button.cancel')}
+      </Button>
+      <Button
+        onClick={desactivate2FA}
+        theme='danger'
+      >
+        {t('ProfileView.twofa.modal.button.desactivate')}
+      </Button>
+    </div>
+  </Fragment>
+))
 
 export default translate()(ProfileView)
