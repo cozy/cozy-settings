3d21f45f820e3b384458d41552c863871c0b80e2
CPatchane <code@patchane.com>
2018-06-08 13:49:49 +0200

    :pencil2: fix: typo in localization contribution link

--- src/components/ProfileView.jsx
+++ src/components/ProfileView.jsx
@@ -98,7 +98,7 @@ class ProfileView extends Component {
           <p>
             <ReactMarkdownWrapper
               source={
-                t('ProfileView.locale.contrib', {link: 'https://support.cozy.io/article/114-doubleauthentification'})
+                t('ProfileView.locale.contrib', {link: 'https://forum.cozy.io/t/how-to-contribute-to-the-cozy-localization/3937'})
               }
             />
           </p>
