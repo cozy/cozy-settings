3aa65aae4f8c0a744f6a6a0b99f972c4c0ef1776
CPatchane <code@patchane.com>
2018-12-14 15:48:57 +0100

    fix: :bug: Markdown link styles

--- src/components/ReactMarkdownWrapper.jsx
+++ src/components/ReactMarkdownWrapper.jsx
@@ -1,18 +1,30 @@
 import React from 'react'
 import ReactMarkdown from 'react-markdown'
 
+import styles from 'styles/index'
+
 const LinkRenderer = props => (
-  <a href={props.href} target="_blank" rel="noopener noreferrer">
+  <a
+    href={props.href}
+    target="_blank"
+    rel="noopener noreferrer"
+    className={styles['set-markdown-link']}
+  >
     {props.children}
   </a>
 )
 
 const reactMarkdownRendererOptions = {
-  Link: LinkRenderer
+  link: LinkRenderer,
+  linkReference: LinkRenderer
 }
 
 const ReactMarkdownWrapper = ({ source }) => (
-  <ReactMarkdown source={source} renderers={reactMarkdownRendererOptions} />
+  <ReactMarkdown
+    source={source}
+    renderers={reactMarkdownRendererOptions}
+    escapeHtml={false}
+  />
 )
 
 export default ReactMarkdownWrapper
