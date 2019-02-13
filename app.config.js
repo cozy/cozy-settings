de74f2b17dcd1d1388a7a0e89b0dd283ccebdcd1
CPatchane <code@patchane.com>
2019-02-13 12:02:49 +0100

    fix: :wrench: Add piwik webpack config

--- app.config.js
+++ app.config.js
@@ -1,6 +1,7 @@
 const configs = [
   require('cozy-scripts/config/webpack.bundle.preact'),
-  require('cozy-scripts/config/webpack.config.css-modules')
+  require('cozy-scripts/config/webpack.config.css-modules'),
+  require('./config/webpack.config.piwik')
 ]
 
 module.exports = configs
