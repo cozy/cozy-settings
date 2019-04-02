bc53bab084629d22a48250332248e3d43536c172
Grégory Le Garec <gregory.legarec@gmail.com>
2019-04-02 17:07:21 +0200

    feat: Extract offers link ✨
    Facilitate customization

---
+++ src/components/OffersLink.jsx
@@ -0,0 +1,36 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import { ButtonLink } from 'cozy-ui/react/Button'
+import { translate } from 'cozy-ui/react/I18n'
+
+import viewStyles from 'styles/view'
+import styles from 'styles/storage'
+
+export class OffersLink extends PureComponent {
+  render() {
+    const { storageData, t } = this.const
+    return storageData.offersLink ? (
+      <div>
+        <h3 className={viewStyles['set-view-subtitle']}>
+          {t('StorageView.more_space')}
+        </h3>
+        <ButtonLink
+          theme="regular"
+          className={styles['set-offer-button']}
+          href={storageData.offersLink}
+          target
+        >
+          {t('StorageView.see_offer')}
+        </ButtonLink>
+      </div>
+    ) : null
+  }
+}
+
+OffersLink.propTypes = {
+  storageData: PropTypes.object.isRequired,
+  t: PropTypes.func.isRequired
+}
+
+export default translate()(OffersLink)
