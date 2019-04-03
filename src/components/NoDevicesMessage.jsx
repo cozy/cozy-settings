9309c9d3b8d8016d191ece4ac302d7c1e446309d
Grégory Le Garec <gregory.legarec@gmail.com>
2019-04-03 12:36:15 +0200

    refactor: Extract NoDeviceMessage component from DevicesView 📝
    For customization purpose.

---
+++ src/components/NoDevicesMessage.jsx
@@ -0,0 +1,31 @@
+import React, { PureComponent } from 'react'
+import PropTypes from 'prop-types'
+
+import Empty from 'cozy-ui/react/Empty'
+import { ButtonLink } from 'cozy-ui/react/Button'
+import { translate } from 'cozy-ui/react/I18n'
+
+import EmptyIcon from 'assets/icons/icon-devices.svg'
+
+export class NoDevicesMessage extends PureComponent {
+  render() {
+    const { t } = this.props
+    return (
+      <Empty
+        icon={EmptyIcon}
+        title={t('Empty.devices.title')}
+        text={t('Empty.devices.text')}
+      >
+        <ButtonLink href={t('Empty.devices.link.href')} target>
+          {t('Empty.devices.link.text')}
+        </ButtonLink>
+      </Empty>
+    )
+  }
+}
+
+NoDevicesMessage.propTypes = {
+  t: PropTypes.func.isRequired
+}
+
+export default translate()(NoDevicesMessage)
