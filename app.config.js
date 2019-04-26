26ae230fee38906f38696778a7d4fc54fec429d2
Grégory Le Garec <gregory.legarec@gmail.com>
2019-04-26 16:42:12 +0200

    chore: Use transpiled cozy-ui 🛠

--- app.config.js
+++ app.config.js
@@ -1,7 +1,8 @@
 const configs = [
   require('cozy-scripts/config/webpack.bundle.default'),
   require('cozy-scripts/config/webpack.config.css-modules'),
-  require('./config/webpack.config.piwik')
+  require('./config/webpack.config.piwik'),
+  require('./config/webpack.config.cozy-ui')
 ]
 
 module.exports = configs
