43890de9fd0351e0baa34202d2e5efea7bc1c236
Patrick Browne <pt.browne@gmail.com>
2019-05-14 15:57:09 +0200

    tests: Add test for two_fa value

---
+++ src/reducers/fields.spec.js
@@ -0,0 +1,61 @@
+import fields from './fields'
+import {
+  CHECK_TWO_FACTOR_CODE_SUCCESS,
+  DESACTIVATE_2FA_SUCCESS,
+  AUTH_MODE
+} from '../actions/twoFactor'
+
+describe('fields reducer', () => {
+  let state
+  beforeEach(() => {
+    const initialState = {}
+    const initialAction = {}
+    state = fields(initialState, initialAction)
+  })
+
+  it('should have a default state', () => {
+    expect(state).toEqual({
+      email: {
+        value: '',
+        submitting: false,
+        saved: false,
+        errors: []
+      },
+      locale: {
+        value: '',
+        submitting: false,
+        saved: false,
+        errors: []
+      },
+      public_name: {
+        value: '',
+        submitting: false,
+        saved: false,
+        errors: []
+      },
+      tracking: {
+        value: '',
+        submitting: false,
+        saved: false,
+        errors: []
+      },
+      auth_mode: {
+        value: '',
+        submitting: false,
+        saved: false,
+        errors: []
+      }
+    })
+  })
+
+  it('should react to two fa actions', () => {
+    state = fields(state, {
+      type: CHECK_TWO_FACTOR_CODE_SUCCESS
+    })
+    expect(state.auth_mode.value).toBe(AUTH_MODE.TWO_FA_MAIL)
+    state = fields(state, {
+      type: DESACTIVATE_2FA_SUCCESS
+    })
+    expect(state.auth_mode.value).toBe(AUTH_MODE.BASIC)
+  })
+})
