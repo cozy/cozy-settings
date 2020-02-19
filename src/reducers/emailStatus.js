183109dbe0841accfa9ec9d19e10dd27d0965db7
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-19 18:43:06 +0100

    fix: Detect en of email sending

--- src/reducers/emailStatus.js
+++ src/reducers/emailStatus.js
@@ -21,6 +21,7 @@ export const isSending = (state = false, action) => {
   switch (action.type) {
     case SEND_EMAIL:
       return true
+    case SEND_EMAIL_SUCCESS:
     case SEND_EMAIL_FAILURE:
       return false
     default:
