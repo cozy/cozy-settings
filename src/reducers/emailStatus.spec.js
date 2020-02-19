fef469fb09ab9c7ad79c3ac10c4ec9528f8716ec
Yannick Lohse <contact@yannick-lohse.fr>
2020-02-19 18:43:06 +0100

    fix: Detect en of email sending

---
+++ src/reducers/emailStatus.spec.js
@@ -0,0 +1,51 @@
+import emailStatusReducer from './emailStatus'
+import {
+  SEND_EMAIL,
+  SEND_EMAIL_SUCCESS,
+  SEND_EMAIL_FAILURE
+} from '../actions/email'
+
+describe('emailStatus reducer', () => {
+  it('should return the initial state', () => {
+    expect(emailStatusReducer(undefined, {})).toEqual({
+      error: null,
+      isSending: false,
+      isSent: false
+    })
+  })
+
+  it('should send an email', () => {
+    let state = emailStatusReducer(state, { type: SEND_EMAIL })
+    expect(state).toEqual({
+      error: null,
+      isSending: true,
+      isSent: false
+    })
+
+    state = emailStatusReducer(state, { type: SEND_EMAIL_SUCCESS })
+    expect(state).toEqual({
+      error: null,
+      isSending: false,
+      isSent: true
+    })
+  })
+
+  it('should handle errors', () => {
+    let state = emailStatusReducer(state, { type: SEND_EMAIL })
+    expect(state).toEqual({
+      error: null,
+      isSending: true,
+      isSent: false
+    })
+
+    state = emailStatusReducer(state, {
+      type: SEND_EMAIL_FAILURE,
+      error: 'No flux capacitator'
+    })
+    expect(state).toEqual({
+      error: 'No flux capacitator',
+      isSending: false,
+      isSent: false
+    })
+  })
+})
