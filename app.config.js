6a7f7f2e49381e305db231068851361b86f46f35
Patrick Browne <pt.browne@gmail.com>
2020-10-15 12:28:46 +0200

    chore: Remove unneeded cozy-ui webpack config

--- app.config.js
+++ app.config.js
@@ -2,7 +2,6 @@ const configs = [
   require('cozy-scripts/config/webpack.bundle.default'),
   require('cozy-scripts/config/webpack.config.css-modules'),
   require('./config/webpack.config.piwik'),
-  require('./config/webpack.config.cozy-ui')
 ]
 
 module.exports = configs
