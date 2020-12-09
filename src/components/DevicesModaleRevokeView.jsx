d5cc5c0feca452cbdeacfa38921c2eb4a5ea9a86
Patrick Browne <pt.browne@gmail.com>
2020-12-09 16:15:46 +0100

    feat: Revoke devices modal -> dialog

--- src/components/DevicesModaleRevokeView.jsx
+++ src/components/DevicesModaleRevokeView.jsx
@@ -1,35 +1,47 @@
 import React from 'react'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
-
-import Modal, { ModalContent } from 'cozy-ui/transpiled/react/Modal'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import { ConfirmDialog } from 'cozy-ui/transpiled/react/CozyDialogs'
+import Button from 'cozy-ui/transpiled/react/Button'
 
 import ReactMarkdown from 'react-markdown'
 
 import classNames from 'classnames'
 import styles from 'styles/devicesModaleRevokeView'
 
-const devicesModaleRevokeView = ({ t, device, revokeDevice, cancelAction }) => {
+const RevokeDeviceDialog = ({ device, revokeDevice, cancelAction }) => {
+  const { t } = useI18n()
   return (
-    <Modal
+    <ConfirmDialog
+      open
       title={t('revokeDevice.title')}
-      primaryText={t('revokeDevice.validate')}
-      primaryType="danger"
-      primaryAction={() => revokeDevice(device.id)}
-      secondaryText="cancel"
-      secondaryAction={() => cancelAction()}
-      dismissAction={() => cancelAction()}
-    >
-      <ModalContent className={styles['coz-modal-revoke-content']}>
-        <ReactMarkdown
-          source={t('revokeDevice.description', { name: device.client_name })}
-        />
-        <ReactMarkdown
-          source={t('revokeDevice.subText')}
-          className={classNames(styles['icon'], styles['icon-arrows'])}
-        />
-      </ModalContent>
-    </Modal>
+      actions={
+        <>
+          <Button
+            label={t('revokeDevice.validate')}
+            theme="danger"
+            onClick={() => revokeDevice(device.id)}
+          />
+          <Button
+            label={t('revokeDevice.cancel')}
+            theme="secondary"
+            onClick={() => cancelAction()}
+          />
+        </>
+      }
+      onClose={() => cancelAction()}
+      content={
+        <>
+          <ReactMarkdown
+            source={t('revokeDevice.description', { name: device.client_name })}
+          />
+          <ReactMarkdown
+            source={t('revokeDevice.subText')}
+            className={classNames(styles['icon'], styles['icon-arrows'])}
+          />
+        </>
+      }
+    />
   )
 }
 
-export default translate()(devicesModaleRevokeView)
+export default RevokeDeviceDialog
