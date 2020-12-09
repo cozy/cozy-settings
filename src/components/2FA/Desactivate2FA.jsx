d8eca7f17f95ac66c9f551cfc399ad28845687cf
Patrick Browne <pt.browne@gmail.com>
2020-12-09 16:15:36 +0100

    feat: Desactivate 2FA use Dialog

--- src/components/2FA/Desactivate2FA.jsx
+++ src/components/2FA/Desactivate2FA.jsx
@@ -1,49 +1,41 @@
-import React, { Fragment } from 'react'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import React from 'react'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { Button } from 'cozy-ui/transpiled/react/Button'
-import Modal, { ModalContent } from 'cozy-ui/transpiled/react/Modal'
-
-import viewStyles from 'styles/view'
+import { ConfirmDialog } from 'cozy-ui/transpiled/react/Modal'
 
 export const Desactivate2FA = ({
-  t,
   desactivate2FA,
   closeTwoFADesactivationModal
-}) => (
-  <div className={viewStyles['set-view-content-twofa-modal-wrapper']}>
-    <Modal
-      dismissAction={closeTwoFADesactivationModal}
-      className={viewStyles['set-view-content-twofa-modal']}
+}) => {
+  const { t } = useI18n()
+  return (
+    <ConfirmDialog
+      onClose={closeTwoFADesactivationModal}
       title={t('ProfileView.twofa.title.desactivate')}
-    >
-      <ModalContent
-        className={viewStyles['set-view-content-twofa-modal-content']}
-      >
-        <Fragment>
+      content={
+        <>
           <p>
             <b>{t('ProfileView.twofa.modal.desactivate_title')}</b>
           </p>
           <p>{t('ProfileView.twofa.modal.desactivate_description')}</p>
-          <div
-            className={
-              viewStyles['set-view-content-twofa-modal-content-right-buttons']
-            }
-          >
-            <Button
-              onClick={closeTwoFADesactivationModal}
-              theme="secondary"
-              label={t('ProfileView.twofa.modal.button.cancel')}
-            />
-            <Button
-              onClick={desactivate2FA}
-              theme="danger"
-              label={t('ProfileView.twofa.modal.button.desactivate')}
-            />
-          </div>
-        </Fragment>
-      </ModalContent>
-    </Modal>
-  </div>
-)
+        </>
+      }
+      actions={
+        <>
+          <Button
+            onClick={closeTwoFADesactivationModal}
+            theme="secondary"
+            label={t('ProfileView.twofa.modal.button.cancel')}
+          />
+          <Button
+            onClick={desactivate2FA}
+            theme="danger"
+            label={t('ProfileView.twofa.modal.button.desactivate')}
+          />
+        </>
+      }
+    />
+  )
+}
 
-export default translate()(Desactivate2FA)
+export default Desactivate2FA
