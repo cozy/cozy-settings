f43cfac89723be03a15360fbd3dedb54c2c9892e
Patrick Browne <pt.browne@gmail.com>
2021-03-17 10:51:18 +0100

    fix: Mock lib/client since there is no DOM in Jest

--- test/app.spec.js
+++ test/app.spec.js
@@ -7,6 +7,11 @@ import { shallow } from 'enzyme'
 
 import { App } from 'components/App'
 
+jest.mock('lib/client', () => {
+  const CozyClient = jest.requireActual('cozy-client').default
+  return new CozyClient({})
+})
+
 describe('App component only', () => {
   it('should be mounted correctly', () => {
     const component = shallow(<App />).getElement()
