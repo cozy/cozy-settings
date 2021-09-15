c8bc4695956b4f06a5b5902995d59f26449d308c
Erwan Guyader <erwan.guyader@cozycloud.cc>
2021-09-15 15:00:44 +0200

    fix: Use ActionMenu in devices view on mobile
      There isn't enough room on a device line in the devices view to
      display both the « Revoke » and the « Configure » button on mobile so
      we replace them with an action menu.
    
      Since the last synchronization date was displayed on two lines which
      was not very elegant, we also move it to the action menu's bottom
      drawer on mobile.

--- src/components/DevicesView.jsx
+++ src/components/DevicesView.jsx
@@ -1,10 +1,18 @@
-import React, { Component } from 'react'
+import compose from 'lodash/flowRight'
+import React, { Component, useCallback, useState } from 'react'
 
 import tableStyles from 'styles/table.styl'
 
 import classNames from 'classnames'
 
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import ActionMenu, {
+  ActionMenuHeader,
+  ActionMenuItem
+} from 'cozy-ui/transpiled/react/ActionMenu'
+import IconButton from 'cozy-ui/transpiled/react/IconButton'
+import DotsIcon from 'cozy-ui/transpiled/react/Icons/Dots'
+import GearIcon from 'cozy-ui/transpiled/react/Icons/Gear'
+import { translate, useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Spinner from 'cozy-ui/transpiled/react/Spinner'
 import MuiButton from 'cozy-ui/transpiled/react/MuiCozyTheme/Buttons'
 import {
@@ -17,6 +25,9 @@ import {
 } from 'cozy-ui/transpiled/react/Table'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import { Media, Img, Bd } from 'cozy-ui/transpiled/react/Media'
+import TrashIcon from 'cozy-ui/transpiled/react/Icons/Trash'
+import Typography from 'cozy-ui/transpiled/react/Typography'
+import withBreakpoints from 'cozy-ui/transpiled/react/helpers/withBreakpoints'
 
 import NoDevicesMessage from 'components/NoDevicesMessage'
 import DevicesModaleRevokeView from 'components/DevicesModaleRevokeView'
@@ -35,6 +46,92 @@ const deviceKindToIcon = {
 
 const getDeviceIcon = device => {
   return deviceKindToIcon[device.client_kind] || laptopIcon
+}
+
+const isDesktopDevice = device => device.client_kind === 'desktop'
+
+const MoreButton = ({ onClick }) => {
+  const { t } = useI18n()
+  return (
+    <IconButton
+      theme="secondary"
+      extension="narrow"
+      size="small"
+      label={t('Toolbar.more')}
+      onClick={onClick}
+    >
+      <Icon icon={DotsIcon} />
+    </IconButton>
+  )
+}
+const MoreMenuItem = ({ onClick, icon, color, text }) => (
+  <ActionMenuItem
+    onClick={onClick}
+    left={<Icon icon={icon} color={`var(--${color}Color)`} />}
+  >
+    <Typography
+      variant="body1"
+      color={color}
+      style={{
+        textTransform: 'capitalize'
+      }}
+    >
+      {text}
+    </Typography>
+  </ActionMenuItem>
+)
+const MoreMenu = ({ device, onRevoke, onConfigure, isMobile }) => {
+  const { f, t } = useI18n()
+
+  const [menuIsVisible, setMenuVisible] = useState(false)
+
+  const openMenu = useCallback(() => setMenuVisible(true), [setMenuVisible])
+  const closeMenu = useCallback(() => setMenuVisible(false), [setMenuVisible])
+  const toggleMenu = useCallback(() => {
+    if (menuIsVisible) return closeMenu()
+    openMenu()
+  }, [closeMenu, openMenu, menuIsVisible])
+  return (
+    <>
+      <MoreButton onClick={toggleMenu} />
+      {isMobile && menuIsVisible && (
+        <ActionMenu onClose={closeMenu} autoclose>
+          <ActionMenuHeader>
+            <Media>
+              <Img>
+                <Icon icon={getDeviceIcon(device)} size={32} />
+              </Img>
+              <Bd className="u-ml-1">
+                <Typography variant="h6">{device.client_name}</Typography>
+                <Typography variant="caption" color="textSecondary">
+                  {t('DevicesView.head_sync')} 
+                  {device.synchronized_at
+                    ? f(
+                        device.synchronized_at,
+                        t('DevicesView.sync_date_format')
+                      )
+                    : '-'}
+                </Typography>
+              </Bd>
+            </Media>
+          </ActionMenuHeader>
+          {isDesktopDevice(device) && (
+            <MoreMenuItem
+              onClick={onConfigure}
+              icon={GearIcon}
+              text={t('DevicesView.configure')}
+            />
+          )}
+          <MoreMenuItem
+            onClick={onRevoke}
+            icon={TrashIcon}
+            color={'error'}
+            text={t('DevicesView.revoke')}
+          />
+        </ActionMenu>
+      )}
+    </>
+  )
 }
 
 class DevicesView extends Component {
@@ -58,7 +155,8 @@ class DevicesView extends Component {
       deviceToRevoke,
       onDeviceModaleRevoke,
       onDeviceModaleRevokeClose,
-      devicePerformRevoke
+      devicePerformRevoke,
+      breakpoints: { isMobile }
     } = this.props
     const { deviceToConfigure } = this.state
     return (
@@ -130,6 +228,18 @@ class DevicesView extends Component {
                         <Icon icon={getDeviceIcon(device)} size={32} />
                       </Img>
                       <Bd className="u-ml-1">{device.client_name}</Bd>
+                      {isMobile && (
+                        <MoreMenu
+                          device={device}
+                          onRevoke={() => {
+                            onDeviceModaleRevoke(device)
+                          }}
+                          onConfigure={() => {
+                            this.setState({ deviceToConfigure: device })
+                          }}
+                          isMobile={true}
+                        />
+                      )}
                     </Media>
                   </TableCell>
                   <TableCell className={tableStyles['set-table-date']}>
@@ -141,24 +251,26 @@ class DevicesView extends Component {
                       : '-'}
                   </TableCell>
                   <TableCell className={tableStyles['set-table-actions']}>
-                    <MuiButton
-                      color="primary"
-                      onClick={() => {
-                        onDeviceModaleRevoke(device)
-                      }}
-                    >
-                      {t('DevicesView.revoke')}
-                    </MuiButton>
-                    {device.client_kind === 'desktop' && (
+                    <>
                       <MuiButton
                         color="primary"
                         onClick={() => {
-                          this.setState({ deviceToConfigure: device })
+                          onDeviceModaleRevoke(device)
                         }}
                       >
-                        {t('DevicesView.configure')}
+                        {t('DevicesView.revoke')}
                       </MuiButton>
-                    )}
+                      {isDesktopDevice(device) && (
+                        <MuiButton
+                          color="primary"
+                          onClick={() => {
+                            this.setState({ deviceToConfigure: device })
+                          }}
+                        >
+                          {t('DevicesView.configure')}
+                        </MuiButton>
+                      )}
+                    </>
                   </TableCell>
                 </TableRow>
               ))}
@@ -170,4 +282,7 @@ class DevicesView extends Component {
   }
 }
 
-export default translate()(DevicesView)
+export default compose(
+  translate(),
+  withBreakpoints()
+)(DevicesView)
