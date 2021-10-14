386a603b5e08bddea695ee985089ae6c566d51ef
Erwan Guyader <erwan.guyader@cozycloud.cc>
2021-10-14 18:17:06 +0200

    feat: Exclude any folder from Desktop clients
      We could exclude folders at the root of the Cozy to be excluded from
      the synchronization on a Cozy Desktop client but could not choose any
      of their subfolders.
    
      We now introduce a TreeView of the existing folders on the Cozy to
      allow for this precision.

--- src/components/DevicesModaleConfigureView.jsx
+++ src/components/DevicesModaleConfigureView.jsx
@@ -14,23 +14,88 @@ import FormControl from 'cozy-ui/transpiled/react/FormControl'
 import FormControlLabel from 'cozy-ui/transpiled/react/FormControlLabel'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import IconButton from 'cozy-ui/transpiled/react/IconButton'
-import List from 'cozy-ui/transpiled/react/MuiCozyTheme/List'
-import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
-import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
-import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import { Media, Img } from 'cozy-ui/transpiled/react/Media'
 import Radio from '@material-ui/core/Radio'
 import RadioGroup from 'cozy-ui/transpiled/react/RadioGroup'
 import Spinner from 'cozy-ui/transpiled/react/Spinner'
 import Typography from 'cozy-ui/transpiled/react/Typography'
+import { TreeItem, TreeView } from 'components/CozyTreeView'
 
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import { useClient } from 'cozy-client'
 
 import {
+  ROOT_FOLDER_ID,
   updateDirectoriesExclusions,
   useFolders
 } from 'lib/deviceConfigurationHelper'
 import logger from 'lib/logger'
+
+const isNotSynchronized = (folder, device) =>
+  folder.not_synchronized_on != null &&
+  folder.not_synchronized_on.some(({ id }) => id === device._id)
+
+const findMixedAncestors = (level, areMixed) => {
+  if (
+    level.parent == null ||
+    level.parent.isExcluded ||
+    areMixed.includes(level.parent)
+  ) {
+    return []
+  } else {
+    return findMixedAncestors(level.parent, areMixed).concat(level.parent)
+  }
+}
+
+const initializeHierarchy = (folders, device) => {
+  let areMixed = []
+
+  const hierarchy = new Map([
+    [ROOT_FOLDER_ID, { _id: ROOT_FOLDER_ID, children: [] }]
+  ])
+  for (const folder of folders) {
+    if (folder._id === ROOT_FOLDER_ID) continue
+
+    const parent = hierarchy.get(folder.dir_id)
+    if (!parent) {
+      throw new Error('Parents must come before their children')
+    }
+
+    const wasExcluded = isNotSynchronized(folder, device)
+    const level = {
+      _id: folder._id,
+      children: [],
+      parent,
+      wasExcluded,
+      isExcluded: wasExcluded,
+      folder
+    }
+    hierarchy.set(level._id, level)
+
+    parent.children.push(level)
+
+    if (level.isExcluded) {
+      areMixed = areMixed.concat(findMixedAncestors(level, areMixed))
+    }
+  }
+
+  // Return root folders
+  return {
+    areMixed,
+    hierarchy
+  }
+}
+
+const levels = hierarchy => Array.from(hierarchy.values())
+
+const ancestors = level =>
+  level.parent != null ? [level.parent].concat(ancestors(level.parent)) : []
+const descendants = level =>
+  level.children.concat(...level.children.map(child => descendants(child)))
+const isChecked = level =>
+  !level.isExcluded && ancestors(level).every(a => !a.isExcluded)
+const isMixed = level =>
+  !level.isExcluded && descendants(level).some(d => d.isExcluded)
 
 const FoldersLoading = () => (
   <div className="u-ta-center">
@@ -50,44 +115,145 @@ const FolderCheckbox = ({ onClick, checked, mixed, disabled, ...props }) => (
   </IconButton>
 )
 
-const FolderRow = ({
-  name,
-  isLastRow,
-  partialSync,
-  isIncluded,
+const FolderLevel = ({
+  level,
+  checked,
+  mixed,
+  disabled,
+  isExpanded,
+  isLast,
   toggleInclusion
-}) => (
-  <ListItem divider={!isLastRow} button onClick={toggleInclusion}>
-    <ListItemIcon>
-      <Icon icon={FileTypeFolderIcon} width="24" height="24" />
-    </ListItemIcon>
-    <ListItemText>
-      <Typography variant="body1">{name}</Typography>
-    </ListItemText>
-    <FolderCheckbox checked={isIncluded} disabled={!partialSync} />
-  </ListItem>
-)
+}) => {
+  const labelStyle = useMemo(
+    () => ({
+      overflow: 'unset',
+      paddingRight: '1rem'
+    }),
+    []
+  )
 
-const FoldersList = ({
-  folders,
-  partialSync,
-  excluded,
-  isIncluded,
-  toggleInclusion,
-  toggleAllInclusion
+  return (
+    <TreeItem
+      divider={!isLast || isExpanded}
+      nodeId={level._id}
+      label={
+        <Media>
+          <Img className="u-pr-half">
+            <FolderCheckbox
+              data-testid={`toggle-${level.folder.name}-inclusion`}
+              checked={checked}
+              mixed={mixed}
+              disabled={disabled}
+              onClick={event => {
+                event.stopPropagation()
+                toggleInclusion(level)
+              }}
+            />
+          </Img>
+          <Img className="u-pr-1">
+            <Icon icon={FileTypeFolderIcon} width="16" height="16" />
+          </Img>
+          <Typography
+            noWrap
+            variant="body1"
+            display="inline"
+            style={labelStyle}
+          >
+            {level.folder.name}
+          </Typography>
+        </Media>
+      }
+    >
+      {level.children.map((child, index) => (
+        <FolderLevel
+          key={child._id}
+          level={child}
+          checked={isChecked(child)}
+          mixed={isMixed(child)}
+          disabled={disabled}
+          isLast={isLast && index === level.children.length - 1}
+          toggleInclusion={toggleInclusion}
+        />
+      ))}
+    </TreeItem>
+  )
+}
+
+const countChecked = levels =>
+  levels.reduce(
+    (count, level) =>
+      count + (level.isExcluded ? 0 : 1) + countChecked(level.children),
+    0
+  )
+
+const FoldersTree = ({
+  disabled,
+  rootLevels,
+  defaultExpanded,
+  setPartialSync
 }) => {
   const { t } = useI18n()
-  const hasFolders = useMemo(() => folders.length > 0, [folders])
-  const classes = useMemo(
-    () => ({
-      root: cx('u-ph-1-half u-fz-tiny', hasFolders && 'u-pv-half'),
-      action: 'u-mv-0'
-    }),
-    [hasFolders]
+
+  const hasLevels = useMemo(() => rootLevels.length > 0, [rootLevels])
+  // XXX: The checkedLevels state is used to force a re-render when an inclusion
+  // toggle is clicked. Otherwise, just updating the levels' data won't be
+  // enough.
+  const [checkedLevels, setCheckedLevels] = useState(countChecked(rootLevels))
+  const [expanded, setExpanded] = useState(defaultExpanded)
+
+  const allExcluded = useMemo(
+    () => checkedLevels === 0 || rootLevels.every(level => level.isExcluded),
+    [checkedLevels, rootLevels]
   )
-  const style = useMemo(
+
+  const toggleAllInclusion = useCallback(() => {
+    if (allExcluded) {
+      rootLevels.forEach(level => {
+        level.isExcluded = false
+      })
+      setCheckedLevels(count => count - rootLevels.length)
+    } else {
+      rootLevels.forEach(level => {
+        level.isExcluded = true
+        descendants(level).forEach(d => {
+          d.isExcluded = false
+        })
+      })
+      setCheckedLevels(count => count + rootLevels.length)
+    }
+    setPartialSync(true)
+  }, [allExcluded, rootLevels, setPartialSync])
+  const toggleInclusion = useCallback(
+    level => {
+      if (isChecked(level)) {
+        level.isExcluded = true
+        descendants(level).forEach(d => {
+          d.isExcluded = false
+        })
+        setCheckedLevels(count => count - 1)
+      } else {
+        level.isExcluded = false
+        ancestors(level).forEach(a => {
+          a.isExcluded = false
+        })
+        setCheckedLevels(count => count + 1)
+      }
+      setPartialSync(true)
+    },
+    [setPartialSync]
+  )
+
+  const headerClasses = useMemo(
     () => ({
-      backgroundColor: 'var(--defaultBackgroundColor)'
+      root: cx('u-pv-0 u-pl-0 u-pr-1-half'),
+      action: 'u-mv-0 u-mr-0'
+    }),
+    []
+  )
+  const headerStyle = useMemo(
+    () => ({
+      backgroundColor: 'var(--defaultBackgroundColor)',
+      flexDirection: 'row-reverse'
     }),
     []
   )
@@ -96,38 +262,50 @@ const FoldersList = ({
     <Card className="u-mt-1 u-p-0">
       <CardHeader
         title={
-          <Typography variant="subtitle1" color="textSecondary">
+          <Typography
+            variant="subtitle1"
+            display="inline"
+            color="textSecondary"
+          >
             {t('configureDevice.my_cozy')}
           </Typography>
         }
         action={
-          hasFolders ? (
+          hasLevels && (
             <FolderCheckbox
               data-testid="toggle-all-inclusion"
-              checked={excluded.length !== folders.length}
-              mixed={excluded.length > 0 && excluded.length !== folders.length}
-              disabled={!partialSync}
+              checked={!allExcluded}
+              mixed={
+                !allExcluded &&
+                rootLevels.some(level => level.isExcluded || isMixed(level))
+              }
+              disabled={disabled}
               onClick={toggleAllInclusion}
             />
-          ) : null
+          )
         }
-        classes={classes}
-        style={style}
+        classes={headerClasses}
+        style={headerStyle}
       />
-      {hasFolders ? (
-        <CardContent className="u-p-0">
-          <List dense>
-            {folders.map((folder, index) => (
-              <FolderRow
-                key={folder.id}
-                name={folder.name}
-                isLastRow={index === folders.length - 1}
-                partialSync={partialSync}
-                isIncluded={isIncluded(folder)}
-                toggleInclusion={() => toggleInclusion(folder)}
+      {hasLevels ? (
+        <CardContent className="u-p-0 u-ov-auto">
+          <TreeView
+            defaultExpanded={defaultExpanded}
+            onNodeToggle={(event, nodeIds) => setExpanded(nodeIds)}
+          >
+            {rootLevels.map((level, index) => (
+              <FolderLevel
+                key={level._id}
+                level={level}
+                checked={isChecked(level)}
+                mixed={isMixed(level)}
+                disabled={disabled}
+                isExpanded={expanded.includes(level._id)}
+                isLast={index === rootLevels.length - 1}
+                toggleInclusion={toggleInclusion}
               />
             ))}
-          </List>
+          </TreeView>
         </CardContent>
       ) : (
         <CardContent className="u-ta-center">
@@ -155,66 +333,44 @@ const ConfigureDeviceSyncDialog = ({
   const client = useClient()
 
   const [partialSync, setPartialSync] = useState(false)
-  const [wasExcluded, setWasExcluded] = useState([])
-  const [excluded, setExcluded] = useState([])
-  const updateExcluded = useCallback(newExcluded => {
-    setExcluded(newExcluded)
-    setPartialSync(true)
-  }, [])
-
+  const [expanded, setExpanded] = useState([])
   const { folders, loading, failed } = useFolders(client)
+  const [hierarchy, setHierarchy] = useState(new Map())
+  const hasFolders = useMemo(() => folders.length > 0, [folders])
+
   useMemo(() => {
     if (failed) {
       Alerter.error(t('configureDevice.load_error'))
     } else if (!loading) {
-      const wasExcluded =
-        folders.length > 0
-          ? folders.filter(
-              folder =>
-                folder.not_synchronized_on != null &&
-                folder.not_synchronized_on.some(({ id }) => id === device.id)
-            )
-          : []
+      const { areMixed, hierarchy } = initializeHierarchy(folders, device)
 
-      if (wasExcluded.length > 0) {
-        setWasExcluded(wasExcluded)
-        updateExcluded(wasExcluded)
-      } else {
-        setExcluded([])
+      setHierarchy(hierarchy)
+
+      if (levels(hierarchy).some(level => level.isExcluded)) {
+        setExpanded(areMixed.map(({ _id }) => _id))
+        setPartialSync(true)
       }
     }
-  }, [device.id, failed, folders, loading, t, updateExcluded])
+  }, [device, failed, folders, loading, t])
 
-  const hasFolders = useMemo(() => folders.length > 0, [folders])
-  const isIncluded = useCallback(
-    folder => !excluded.some(f => f.id === folder.id),
-    [excluded]
-  )
-  const toggleAllInclusion = useCallback(
-    () => updateExcluded(excluded.length === folders.length ? [] : folders),
-    [excluded, folders, updateExcluded]
-  )
-  const toggleInclusion = useCallback(
-    folder => {
-      updateExcluded(
-        isIncluded(folder, excluded)
-          ? excluded.concat(folder)
-          : excluded.filter(f => f.id !== folder.id)
-      )
-    },
-    [excluded, isIncluded, updateExcluded]
-  )
   const onPartialSyncChange = useCallback(
     event => setPartialSync(event.target.value === 'true'),
     [setPartialSync]
   )
+
   const configureDevice = useCallback(async () => {
     const foldersToExclude = partialSync
-      ? excluded.filter(id => !wasExcluded.includes(id))
+      ? levels(hierarchy)
+          .filter(level => level.isExcluded && !level.wasExcluded)
+          .map(level => level.folder)
       : []
     const foldersToInclude = partialSync
-      ? wasExcluded.filter(id => !excluded.includes(id))
-      : wasExcluded
+      ? levels(hierarchy)
+          .filter(level => level.wasExcluded && !level.isExcluded)
+          .map(level => level.folder)
+      : levels(hierarchy)
+          .filter(level => level.wasExcluded)
+          .map(level => level.folder)
 
     try {
       await updateDirectoriesExclusions({
@@ -229,15 +385,7 @@ const ConfigureDeviceSyncDialog = ({
     }
 
     onDeviceConfigured()
-  }, [
-    device,
-    partialSync,
-    excluded,
-    wasExcluded,
-    onDeviceConfigured,
-    client,
-    t
-  ])
+  }, [partialSync, hierarchy, onDeviceConfigured, device, client, t])
 
   return (
     <FixedDialog
@@ -283,16 +431,16 @@ const ConfigureDeviceSyncDialog = ({
                 />
               </RadioGroup>
             </FormControl>
-            <>
-              <FoldersList
-                partialSync={partialSync}
-                folders={folders}
-                excluded={excluded}
-                isIncluded={isIncluded}
-                toggleInclusion={toggleInclusion}
-                toggleAllInclusion={toggleAllInclusion}
-              />
-            </>
+            <FoldersTree
+              disabled={!partialSync}
+              rootLevels={
+                hierarchy.has(ROOT_FOLDER_ID)
+                  ? hierarchy.get(ROOT_FOLDER_ID).children
+                  : []
+              }
+              defaultExpanded={expanded}
+              setPartialSync={setPartialSync}
+            />
           </>
         )
       }
