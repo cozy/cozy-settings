f8d2cdd59f2ac620c225762333df3561061514d6
JF-Cozy <julien.ficheux@cozycloud.cc>
2022-08-25 11:43:56 +0200

    refactor: Replace class comp by func comp for DeleteAccount
    no code change

--- src/components/DeleteAccount.jsx
+++ src/components/DeleteAccount.jsx
@@ -1,9 +1,9 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 
-import { translate } from 'cozy-ui/transpiled/react/I18n'
 import Alerter from 'cozy-ui/transpiled/react/Alerter'
 import Button from 'cozy-ui/transpiled/react/Button'
 import Typography from 'cozy-ui/transpiled/react/Typography'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import ConfirmModal from 'components/DeleteAccount/ConfirmModal'
 import FormModal from 'components/DeleteAccount/FormModal'
@@ -12,71 +12,54 @@ const CONFIRMING = 'confirming'
 const IDLE = 'idle'
 const REQUESTING = 'requesting'
 
-export class DeleteAccount extends Component {
-  state = {
-    status: IDLE
-  }
+const DeleteAccount = () => {
+  const { t } = useI18n()
+  const [status, setStatus] = useState(IDLE)
 
-  cancel = () => {
-    this.setStatus(IDLE)
-  }
-
-  confirm = () => this.setStatus(CONFIRMING)
-
-  request = () => this.setStatus(REQUESTING)
-
-  setStatus = status => this.setState({ status })
-
-  onError = error => {
-    const { t } = this.props
-    this.setStatus(IDLE)
+  const onError = error => {
+    setStatus(IDLE)
     console.error(error.message) // eslint-disable-line no-console
     Alerter.error(t('DeleteAccount.error.message'))
   }
 
-  onRequested = () => {
-    const { t } = this.props
-    this.setStatus(IDLE)
+  const onRequested = () => {
+    setStatus(IDLE)
     Alerter.success(t('DeleteAccount.success.message'))
   }
 
-  render = () => {
-    const { t } = this.props
-    const { status } = this.state
-    return (
-      <>
-        {status === CONFIRMING && (
-          <ConfirmModal
-            dismissAction={this.cancel}
-            primaryAction={this.request}
+  return (
+    <>
+      {status === CONFIRMING && (
+        <ConfirmModal
+          dismissAction={() => setStatus(IDLE)}
+          primaryAction={() => setStatus(REQUESTING)}
+        />
+      )}
+      {status === REQUESTING && (
+        <FormModal
+          onClose={() => setStatus(IDLE)}
+          onError={onError}
+          onSuccess={onRequested}
+        />
+      )}
+      <div>
+        <Typography variant="h5" gutterBottom>
+          {t('DeleteAccount.title')}
+        </Typography>
+        <Typography variant="body1">{t('DeleteAccount.label')}</Typography>
+        <div className="u-mt-1">
+          <Button
+            busy={status === REQUESTING}
+            disabled={status !== IDLE}
+            extension="full"
+            label={t('DeleteAccount.button.label')}
+            onClick={() => setStatus(CONFIRMING)}
+            theme="danger-outline"
           />
-        )}
-        {status === REQUESTING && (
-          <FormModal
-            onClose={this.cancel}
-            onError={this.onError}
-            onSuccess={this.onRequested}
-          />
-        )}
-        <div>
-          <Typography variant="h5" gutterBottom>
-            {t('DeleteAccount.title')}
-          </Typography>
-          <Typography variant="body1">{t('DeleteAccount.label')}</Typography>
-          <div className="u-mt-1">
-            <Button
-              busy={status === REQUESTING}
-              disabled={status !== IDLE}
-              extension="full"
-              label={t('DeleteAccount.button.label')}
-              onClick={this.confirm}
-              theme="danger-outline"
-            />
-          </div>
         </div>
-      </>
-    )
-  }
+      </div>
+    </>
+  )
 }
 
-export default translate()(DeleteAccount)
+export default DeleteAccount
