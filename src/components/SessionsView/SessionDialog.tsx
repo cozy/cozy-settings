5450d57fb943c49dd114d41eb26d64161fe7002f
Cozy Pierre <pierre.trolle@cozycloud.cc>
2022-09-28 16:12:49 +0200

    feat(sessions): add responsive view on mobile

---
+++ src/components/SessionsView/SessionDialog.tsx
@@ -0,0 +1,41 @@
+import { UAParser } from 'ua-parser-js'
+import React from 'react'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import tableStyles from 'styles/table.styl'
+
+interface Session {
+  user_agent: string
+  created_at: string
+  os: string
+  browser: { name: string; major: string }
+  ip: string
+}
+
+interface Prop {
+  session: Session
+}
+
+const SessionDialog = ({ session }: Prop): JSX.Element => {
+  const { f, t } = useI18n()
+  const ua = UAParser(session.user_agent)
+  return (
+    <span className={tableStyles['dialog-container']}>
+      <p className={tableStyles['dialog-paragraph']}>
+        {t('SessionsView.modal_date')}{' '}
+        {/* eslint-disable-next-line @typescript-eslint/no-unsafe-call*/}
+        {f(new Date(session.created_at), t('SessionsView.sync_date_format'))}
+      </p>
+      <p className={tableStyles['dialog-paragraph']}>
+        {t('SessionsView.modal_os')} {session.os}
+      </p>
+      <p className={tableStyles['dialog-paragraph']}>
+        {t('SessionsView.modal_browser')} {ua.browser.name} {ua.browser.major}
+      </p>
+      <p className={tableStyles['dialog-paragraph']}>
+        {t('SessionsView.modal_ip')} {session.ip}
+      </p>
+    </span>
+  )
+}
+
+export default SessionDialog
--- src/components/SessionsViewRow.jsx
+++ src/components/SessionsViewRow.jsx
@@ -4,11 +4,77 @@ import React from 'react'
 import { UAParser } from 'ua-parser-js'
 
 import { TableRow, TableCell } from 'cozy-ui/transpiled/react/Table'
-import { useI18n } from 'cozy-ui/transpiled/react'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import BrowserBraveIcon from 'cozy-ui/transpiled/react/Icons/BrowserBrave'
+import BrowserChromeIcon from 'cozy-ui/transpiled/react/Icons/BrowserChrome'
+import BrowserDuckduckgoIcon from 'cozy-ui/transpiled/react/Icons/BrowserDuckduckgo'
+import BrowserEdgeIcon from 'cozy-ui/transpiled/react/Icons/BrowserEdge'
+import BrowserFirefoxIcon from 'cozy-ui/transpiled/react/Icons/BrowserFirefox'
+import BrowserIeIcon from 'cozy-ui/transpiled/react/Icons/BrowserIe'
+import BrowserSafariIcon from 'cozy-ui/transpiled/react/Icons/BrowserSafari'
+import DeviceBrowserIcon from 'cozy-ui/transpiled/react/Icons/DeviceBrowser'
+import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+import Icon from 'cozy-ui/transpiled/react/Icon'
 
-const SessionsViewRow = ({ session }) => {
+const getBrowserIcon = session => {
+  const ua = UAParser(session.user_agent)
+
+  if (ua.browser.name === 'Brave') {
+    return BrowserBraveIcon
+  }
+  if (ua.browser.name.startsWith('Chrom')) {
+    return BrowserChromeIcon
+  }
+  if (ua.browser.name === 'DuckDuckGo') {
+    return BrowserDuckduckgoIcon
+  }
+  if (ua.browser.name === 'Edge') {
+    return BrowserEdgeIcon
+  }
+  if (ua.browser.name.startsWith('Firefox')) {
+    return BrowserFirefoxIcon
+  }
+  if (ua.browser.name === 'IE') {
+    return BrowserIeIcon
+  }
+  if (ua.browser.name === 'Safari') {
+    return BrowserSafariIcon
+  }
+  return DeviceBrowserIcon
+}
+
+const SessionsViewRow = ({ session, displayModal }) => {
+  const { isMobile } = useBreakpoints()
   const { f, t } = useI18n()
   const ua = UAParser(session.user_agent)
+
+  const callRow = () => {
+    displayModal(session)
+  }
+  if (isMobile) {
+    return (
+      <TableRow className={tableStyles['row']} onClick={callRow}>
+        <TableCell className={tableStyles['browser']}>
+          <Icon
+            icon={getBrowserIcon(session)}
+            size={16}
+            color="var(--textIconColor)"
+          />
+        </TableCell>
+        <TableCell className={tableStyles['main']}>
+          <span className={tableStyles['title']}>
+            {`${ua.browser.name} · ${session.os}`}
+          </span>
+          <span className={tableStyles['subtitle']}>
+            {f(
+              new Date(session.created_at),
+              t('SessionsView.sync_date_format')
+            )}
+          </span>
+        </TableCell>
+      </TableRow>
+    )
+  }
   return (
     <TableRow>
       <TableCell className={tableStyles['set-table-date']}>
