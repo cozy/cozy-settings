310fd5b7870e4a65c6c3a50fac57b6571d51d174
Pierre Recontact <commit@recontact.me>
2022-09-30 15:18:21 +0200

    wip

--- src/components/SessionsView/SessionsViewRow.jsx
+++ src/components/SessionsView/SessionsViewRow.jsx
@@ -15,6 +15,13 @@ import BrowserSafariIcon from 'cozy-ui/transpiled/react/Icons/BrowserSafari'
 import DeviceBrowserIcon from 'cozy-ui/transpiled/react/Icons/DeviceBrowser'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
 import Icon from 'cozy-ui/transpiled/react/Icon'
+import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
+import ListItemIcon from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemIcon'
+import ListItemText from 'cozy-ui/transpiled/react/ListItemText'
+import SyncIcon from 'cozy-ui/transpiled/react/Icons/Sync'
+import ListItemSecondaryAction from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItemSecondaryAction'
+import MoreMenu from '../DevicesView/DevicesViewMoreMenu'
+import Divider from 'cozy-ui/transpiled/react/MuiCozyTheme/Divider'
 
 const getBrowserIcon = session => {
   const ua = UAParser(session.user_agent)
@@ -51,46 +58,66 @@ const SessionsViewRow = ({ session, displayModal }) => {
   const callRow = () => {
     displayModal(session)
   }
-  if (isMobile) {
-    return (
-      <TableRow className={tableStyles['row']} onClick={callRow}>
-        <TableCell className={tableStyles['browser']}>
-          <Icon
-            icon={getBrowserIcon(session)}
-            size={16}
-            color="var(--textIconColor)"
-          />
-        </TableCell>
-        <TableCell className={tableStyles['main']}>
-          <span className={tableStyles['title']}>
-            {`${ua.browser.name} · ${session.os}`}
-          </span>
-          <span className={tableStyles['subtitle']}>
-            {f(
-              new Date(session.created_at),
-              t('SessionsView.sync_date_format')
-            )}
-          </span>
-        </TableCell>
-      </TableRow>
-    )
-  }
+
   return (
-    <TableRow>
-      <TableCell className={tableStyles['set-table-date']}>
-        {f(new Date(session.created_at), t('SessionsView.sync_date_format'))}
-      </TableCell>
-      <TableCell className={tableStyles['set-table-os']}>
-        {session.os}
-      </TableCell>
-      <TableCell className={tableStyles['set-table-browser']}>
-        {ua.browser.name} {ua.browser.major}
-      </TableCell>
-      <TableCell className={tableStyles['set-table-ip']}>
-        {session.ip}
-      </TableCell>
-    </TableRow>
+    <>
+      <ListItem button key={session.created_at}>
+        <ListItemIcon>
+          <Icon icon={getBrowserIcon(session)} size={32} />
+        </ListItemIcon>
+        <ListItemText primary={`${ua.browser.name} · ${session.os}`} />
+        <ListItemText
+          secondary={f(
+            new Date(session.created_at),
+            t('SessionsView.sync_date_format')
+          )}
+        />
+        <ListItemText secondary={session.ip}></ListItemText>
+      </ListItem>
+      <Divider component="li" variant="inset" />
+    </>
   )
+
+  // if (isMobile) {
+  //   return (
+  //     <TableRow className={tableStyles['row']} onClick={callRow}>
+  //       <TableCell className={tableStyles['browser']}>
+  //         <Icon
+  //           icon={getBrowserIcon(session)}
+  //           size={16}
+  //           color="var(--textIconColor)"
+  //         />
+  //       </TableCell>
+  //       <TableCell className={tableStyles['main']}>
+  //         <span className={tableStyles['title']}>
+  //           {`${ua.browser.name} · ${session.os}`}
+  //         </span>
+  //         <span className={tableStyles['subtitle']}>
+  //           {f(
+  //             new Date(session.created_at),
+  //             t('SessionsView.sync_date_format')
+  //           )}
+  //         </span>
+  //       </TableCell>
+  //     </TableRow>
+  //   )
+  // }
+  // return (
+  //   <TableRow>
+  //     <TableCell className={tableStyles['set-table-date']}>
+  //       {f(new Date(session.created_at), t('SessionsView.sync_date_format'))}
+  //     </TableCell>
+  //     <TableCell className={tableStyles['set-table-os']}>
+  //       {session.os}
+  //     </TableCell>
+  //     <TableCell className={tableStyles['set-table-browser']}>
+  //       {ua.browser.name} {ua.browser.major}
+  //     </TableCell>
+  //     <TableCell className={tableStyles['set-table-ip']}>
+  //       {session.ip}
+  //     </TableCell>
+  //   </TableRow>
+  // )
 }
 
 export default SessionsViewRow
