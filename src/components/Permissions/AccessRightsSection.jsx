5abb86287e67f90ccec9d909df9ff86a3be5e9e9
Ldoppea <yannick.chiron@gmail.com>
2023-01-11 09:43:55 +0100

    feat: Add modal for AppPermissions elements
    also change color of the divider
    
    Co-authored-by: vdnj <61142137+vdnj@users.noreply.github.com>

--- src/components/Permissions/AccessRightsSection.jsx
+++ src/components/Permissions/AccessRightsSection.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NavigationListSection } from 'cozy-ui/transpiled/react/NavigationList'
 import ListItem from 'cozy-ui/transpiled/react/MuiCozyTheme/ListItem'
 import Typography from 'cozy-ui/transpiled/react/Typography'
@@ -18,9 +18,10 @@ import {
   displayPermissions,
   getPermissionIconName
 } from './helpers/permissionsHelper'
-import { routes } from 'constants/routes'
 import withAllLocales from '../../lib/withAllLocales'
 import useBreakpoints from 'cozy-ui/transpiled/react/hooks/useBreakpoints'
+import { Dialog as DialogComponent } from 'cozy-ui/transpiled/react/CozyDialogs'
+import Alert from 'cozy-ui/transpiled/react/Alert'
 
 const AccessRightsSection = ({
   sortedPermissionsByName,
@@ -28,7 +29,17 @@ const AccessRightsSection = ({
   t,
   isRemoteDoctypes
 }) => {
+  const [modalOpened, setModalOpened] = useState(false)
+  const [modalData, setModalData] = useState()
   const { isMobile, isTablet } = useBreakpoints()
+
+  const openModal = data => {
+    setModalData(data)
+    setModalOpened(true)
+  }
+
+  const handleClose = () => setModalOpened(false)
+
   if (!sortedPermissionsByName || sortedPermissionsByName.length < 1) {
     return
   }
@@ -36,7 +47,7 @@ const AccessRightsSection = ({
     <div
       style={
         isMobile || isTablet
-          ? { borderTop: '16px solid var(--dividerColor)' }
+          ? { borderTop: '16px solid var(--defaultBackgroundColor)' }
           : {}
       }
     >
@@ -64,44 +75,126 @@ const AccessRightsSection = ({
           />
         </ListItem>
         <Divider />
-        {sortedPermissionsByName.map(({ name, title, verbs, type }, index) => {
-          const iconName = getPermissionIconName(type)
-          return (
-            <div key={name}>
-              <ListItem
-                button
-                component="a"
-                href={`/#${routes.appList}/${slugName}/${name}`}
-              >
-                <ListItemIcon>
-                  <Icon
-                    icon={
-                      require(`cozy-ui/transpiled/react/Icons/${iconName}`)
-                        .default
-                    }
-                    size={mediumSize}
+        {sortedPermissionsByName.map(
+          ({ description, name, title, verbs, type }, index) => {
+            const iconName = getPermissionIconName(type)
+            return (
+              <div key={name}>
+                <ListItem
+                  button
+                  component="a"
+                  onClick={() =>
+                    openModal({
+                      description,
+                      name,
+                      title,
+                      verbs,
+                      type,
+                      isRemoteDoctypes
+                    })
+                  }
+                >
+                  <ListItemIcon>
+                    <Icon
+                      icon={
+                        require(`cozy-ui/transpiled/react/Icons/${iconName}`)
+                          .default
+                      }
+                      size={mediumSize}
+                    />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={title}
+                    secondary={t(displayPermissions(verbs))}
                   />
-                </ListItemIcon>
-                <ListItemText
-                  primary={title}
-                  secondary={t(displayPermissions(verbs))}
-                />
-                <ListItemSecondaryAction>
-                  <Icon
-                    icon={RightIcon}
-                    size={smallSize}
-                    className="u-mr-1"
-                    style={{ color: 'var(--secondaryTextColor)' }}
-                  />
-                </ListItemSecondaryAction>
-              </ListItem>
-              {index !== sortedPermissionsByName.length - 1 && (
-                <Divider variant="inset" />
-              )}
-            </div>
-          )
-        })}
+                  <ListItemSecondaryAction>
+                    <Icon
+                      icon={RightIcon}
+                      size={smallSize}
+                      className="u-mr-1"
+                      style={{ color: 'var(--secondaryTextColor)' }}
+                    />
+                  </ListItemSecondaryAction>
+                </ListItem>
+                {index !== sortedPermissionsByName.length - 1 && (
+                  <Divider variant="inset" />
+                )}
+              </div>
+            )
+          }
+        )}
       </NavigationListSection>
+      <DialogComponent
+        open={modalOpened}
+        onClose={handleClose}
+        title={modalData?.title ? modalData.title : ''}
+        content={
+          <>
+            <Typography variant="h4">
+              {modalData
+                ? modalData.isRemoteDoctypes
+                  ? t('Permissions.exit_right', {
+                      app: slugName.toUpperCase(),
+                      doctype: modalData.title.toLowerCase()
+                    })
+                  : t('Permissions.access_right', {
+                      app: slugName.toUpperCase(),
+                      doctype: modalData.title.toLowerCase()
+                    })
+                : ''}
+            </Typography>
+            <Alert
+              severity={modalData?.isRemoteDoctypes ? 'warning' : 'success'}
+              className="u-mv-1"
+            >
+              {modalData?.isRemoteDoctypes
+                ? t('Permissions.exit_right_description')
+                : t('Permissions.access_right_description', {
+                    app: slugName.toUpperCase()
+                  })}
+            </Alert>
+            <Typography variant="h4">
+              {t('Permissions.right_reason', { app: slugName.toUpperCase() })}
+            </Typography>
+            <p>{modalData?.description}</p>
+            <Typography variant="h4" className="u-mv-1">
+              {t('Permissions.details')}
+            </Typography>
+            {modalData &&
+            displayPermissions(modalData.verbs) === 'Permissions.read' ? (
+              <>
+                <Typography variant="h5">
+                  {t('Permissions.read_right_title')}
+                </Typography>
+                <p>{t('Permissions.read_right_text')}</p>
+              </>
+            ) : modalData &&
+              displayPermissions(modalData.verbs) === 'Permissions.write' ? (
+              <>
+                <Typography variant="h5">
+                  {t('Permissions.write_right_title')}
+                </Typography>
+                <p>{t('Permissions.write_right_text')}</p>
+              </>
+            ) : (
+              <>
+                <Typography variant="h5">
+                  {t('Permissions.read_right_title')}
+                </Typography>
+                <p style={{ color: 'var(--secondaryTextColor)' }}>
+                  {t('Permissions.read_right_text')}
+                </p>
+                <Typography variant="h5">
+                  {t('Permissions.write_right_title')}
+                </Typography>
+                <p style={{ color: 'var(--secondaryTextColor)' }}>
+                  {t('Permissions.write_right_text')}
+                </p>
+              </>
+            )}
+          </>
+        }
+      />
     </div>
   )
 }
