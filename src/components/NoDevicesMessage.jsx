d74d7e1f58f9e6976c9dfd5d2f3aab47626c1751
cballevre <celestin.ballevre@protonmail.com>
2023-06-20 14:45:50 +0200

    refactor(NoDevicesMessage): Convert from class to functional component

--- src/components/NoDevicesMessage.jsx
+++ src/components/NoDevicesMessage.jsx
@@ -1,32 +1,28 @@
-import React, { PureComponent } from 'react'
-import PropTypes from 'prop-types'
+import React from 'react'
 
 import Empty from 'cozy-ui/transpiled/react/Empty'
 import { ButtonLink } from 'cozy-ui/transpiled/react/Button'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import EmptyIcon from 'assets/icons/icon-devices.svg'
 
-export class NoDevicesMessage extends PureComponent {
-  render() {
-    const { t } = this.props
-    return (
-      <Empty
-        icon={EmptyIcon}
-        title={t('Empty.devices.title')}
-        text={t('Empty.devices.text')}
-      >
-        <ButtonLink
-          href={t('Empty.devices.link.href')}
-          label={t('Empty.devices.link.text')}
-        />
-      </Empty>
-    )
-  }
+/**
+ * Empty state to be displayed when no device is connected
+ */
+const NoDevicesMessage = () => {
+  const { t } = useI18n()
+  return (
+    <Empty
+      icon={EmptyIcon}
+      title={t('Empty.devices.title')}
+      text={t('Empty.devices.text')}
+    >
+      <ButtonLink
+        href={t('Empty.devices.link.href')}
+        label={t('Empty.devices.link.text')}
+      />
+    </Empty>
+  )
 }
 
-NoDevicesMessage.propTypes = {
-  t: PropTypes.func.isRequired
-}
-
-export default translate()(NoDevicesMessage)
+export default NoDevicesMessage
