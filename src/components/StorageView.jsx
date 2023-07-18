b16520295a3cf64ea993cc33bfa8bd57b24bb52d
cballevre <celestin.ballevre@protonmail.com>
2023-07-18 14:41:42 +0200

    refactor(StorageView): Convert to functional component

--- src/components/StorageView.jsx
+++ src/components/StorageView.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import classNames from 'classnames'
 
 import Spinner from 'cozy-ui/transpiled/react/Spinner'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { isFlagshipApp } from 'cozy-device-helper'
-import { translate } from 'cozy-ui/transpiled/react/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 
 import styles from 'styles/storage.styl'
 import OffersLink from 'components/OffersLink'
@@ -13,61 +13,61 @@ import Page from 'components/Page'
 import PageTitle from 'components/PageTitle'
 import { makeDiskInfos } from 'lib/makeDiskInfos'
 
-class StorageView extends Component {
-  UNSAFE_componentWillMount() {
-    this.props.fetchStorageData()
-  }
+const StorageView = ({ isFetching, storageData, fetchStorageData }) => {
+  const { t } = useI18n()
 
-  render() {
-    const { t, isFetching, storageData } = this.props
-    const { humanDiskQuota, humanDiskUsage, percentUsage } = makeDiskInfos(
-      storageData.usage,
-      storageData.quota
-    )
+  useEffect(() => {
+    fetchStorageData()
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [])
 
-    return (
-      <Page narrow>
-        <PageTitle>{t('StorageView.title')}</PageTitle>
-        <Typography variant="h5" gutterBottom>
-          {t('StorageView.storage_title')}
-        </Typography>
-        {isFetching && (
-          <Spinner
-            className="u-pos-fixed-s"
-            middle
-            size="xxlarge"
-            loadingType="loading"
+  const { humanDiskQuota, humanDiskUsage, percentUsage } = makeDiskInfos(
+    storageData.usage,
+    storageData.quota
+  )
+
+  return (
+    <Page narrow>
+      <PageTitle>{t('StorageView.title')}</PageTitle>
+      <Typography variant="h5" gutterBottom>
+        {t('StorageView.storage_title')}
+      </Typography>
+      {isFetching && (
+        <Spinner
+          className="u-pos-fixed-s"
+          middle
+          size="xxlarge"
+          loadingType="loading"
+        />
+      )}
+      {!isFetching && storageData && (
+        <div>
+          <Typography variant="h4" gutterBottom>
+            {t('StorageView.storage_phrase', {
+              humanDiskUsage,
+              humanDiskQuota
+            })}
+          </Typography>
+          <progress
+            className={styles['set-storage-bar']}
+            value={humanDiskUsage}
+            max={humanDiskQuota}
+            min="0"
           />
-        )}
-        {!isFetching && storageData && (
-          <div>
-            <Typography variant="h4" gutterBottom>
-              {t('StorageView.storage_phrase', {
-                humanDiskUsage,
-                humanDiskQuota
-              })}
-            </Typography>
-            <progress
-              className={styles['set-storage-bar']}
-              value={humanDiskUsage}
-              max={humanDiskQuota}
-              min="0"
-            />
-            <span
-              className={classNames(
-                styles['set-bar-percent'],
-                +percentUsage < 5 ? styles['--dark'] : ''
-              )}
-            >
-              {`${percentUsage}%`}
-            </span>
+          <span
+            className={classNames(
+              styles['set-bar-percent'],
+              +percentUsage < 5 ? styles['--dark'] : ''
+            )}
+          >
+            {`${percentUsage}%`}
+          </span>
 
-            {!isFlagshipApp() && <OffersLink storageData={storageData} />}
-          </div>
-        )}
-      </Page>
-    )
-  }
+          {!isFlagshipApp() && <OffersLink storageData={storageData} />}
+        </div>
+      )}
+    </Page>
+  )
 }
 
-export default translate()(StorageView)
+export default StorageView
