652b803a2807c3891a0d753918c14d0d92354394
cballevre <celestin.ballevre@protonmail.com>
2023-07-20 16:59:25 +0200

    feat(Subcription): Show the page on FlagshipApp

--- src/components/Subscription/SubscriptionMenuItem.jsx
+++ src/components/Subscription/SubscriptionMenuItem.jsx
@@ -1,19 +1,21 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 
 import flag from 'cozy-flags'
 import CozyCircle from 'cozy-ui/transpiled/react/Icons/CozyCircle'
 import { useI18n } from 'cozy-ui/transpiled/react/I18n'
+import { isFlagshipApp } from 'cozy-device-helper'
 
 import { MenuItemAnchor } from 'components/menu/MenuItemAnchor'
 import { MenuItemNavLink } from 'components/menu/MenuItemNavLink'
 import { routes } from 'constants/routes'
+import { useOffersLink } from 'hooks/useOffersLink'
 
 /**
  * Sidebar menu link for plan
  */
-const SubscriptionMenuItem = ({ offersLink }) => {
+const SubscriptionMenuItem = () => {
   const { t } = useI18n()
+  const offersLink = useOffersLink()
 
   if (flag('settings.subscription')) {
     return (
@@ -25,19 +27,18 @@ const SubscriptionMenuItem = ({ offersLink }) => {
     )
   }
 
-  return (
-    <MenuItemAnchor
-      primary={t('Nav.primary_plan')}
-      href={offersLink}
-      target="_blank"
-      icon={CozyCircle}
-    />
-  )
-}
+  if (offersLink && !isFlagshipApp()) {
+    return (
+      <MenuItemAnchor
+        primary={t('Nav.primary_plan')}
+        href={offersLink}
+        target="_blank"
+        icon={CozyCircle}
+      />
+    )
+  }
 
-SubscriptionMenuItem.propTypes = {
-  /** Link to our plans page */
-  offersLink: PropTypes.func.isRequired
+  return null
 }
 
 export { SubscriptionMenuItem }
