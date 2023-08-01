2c3f0780bc54377053873cba981c041ef8067a9d
cballevre <celestin.ballevre@protonmail.com>
2023-08-01 11:47:47 +0200

    refactor(Devices): Extract buildDevicesQuery into queries

--- src/lib/queries.js
+++ src/lib/queries.js
@@ -3,7 +3,8 @@ import {
   APPS_DOCTYPE,
   KONNECTORS_DOCTYPE,
   REMOTE_REQUEST_DOCTYPES,
-  SETTINGS_DOCTYPE
+  SETTINGS_DOCTYPE,
+  OAUTH_CLIENTS_DOCTYPE
 } from 'doctypes'
 
 const FIVE_MINUTES = 5 * 60 * 1000
@@ -84,5 +85,13 @@ export const buildContextQuery = () => ({
     as: `${SETTINGS_DOCTYPE}/context`,
     fetchPolicy: CozyClient.fetchPolicies.olderThan(FIVE_MINUTES),
     singleDocData: true
+  }
+})
+
+const DEVICES_QUERY_LIMIT = 1000
+export const buildDevicesQuery = () => ({
+  definition: () => Q(OAUTH_CLIENTS_DOCTYPE).limitBy(DEVICES_QUERY_LIMIT),
+  options: {
+    as: `${OAUTH_CLIENTS_DOCTYPE} _id asc`
   }
 })
