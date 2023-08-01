a4618473147be38e4819caa4af625328df9c26dc
cballevre <celestin.ballevre@protonmail.com>
2023-08-01 11:44:17 +0200

    refactor(DevicesView): Extract into its own folder

--- src/containers/Devices.jsx
+++ src/containers/Devices.jsx
@@ -2,7 +2,7 @@ import { connect } from 'react-redux'
 
 import { translate } from 'cozy-ui/transpiled/react/I18n'
 
-import DevicesView from 'components/DevicesView'
+import { DevicesView } from 'components/Devices/DevicesView'
 
 const mapStateToProps = state => ({
   openDeviceRevokeModale: state.openDeviceRevokeModale,
