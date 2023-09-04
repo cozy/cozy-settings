6772c45729b91fd755e00c890a1ed1ef43187c71
cballevre <celestin.ballevre@protonmail.com>
2023-09-04 16:52:41 +0200

    refactor(TrackingSection): Convert to functional component

--- src/components/Profile/TrackingSection.jsx
+++ src/components/Profile/TrackingSection.jsx
@@ -1,28 +1,27 @@
-import React, { PureComponent } from 'react'
+import React from 'react'
 
-import { translate } from 'cozy-ui/transpiled/react/providers/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import Input from 'components/Input'
 
-export class TrackingSection extends PureComponent {
-  render() {
-    const { instance, fields, onChange, t } = this.props
-    return (
-      <Input
-        name="tracking"
-        type="checkbox"
-        title={t('ProfileView.tracking.title')}
-        label={t('ProfileView.tracking.label', {
-          version:
-            instance && instance.data.attributes.tos
-              ? `-${instance.data.attributes.tos}`
-              : '-201711'
-        })}
-        {...fields.tracking}
-        onChange={onChange}
-      />
-    )
-  }
+export const TrackingSection = ({ instance, fields, onChange }) => {
+  const { t } = useI18n()
+
+  return (
+    <Input
+      name="tracking"
+      type="checkbox"
+      title={t('ProfileView.tracking.title')}
+      label={t('ProfileView.tracking.label', {
+        version:
+          instance && instance.data.attributes.tos
+            ? `-${instance.data.attributes.tos}`
+            : '-201711'
+      })}
+      {...fields.tracking}
+      onChange={onChange}
+    />
+  )
 }
 
-export default translate()(TrackingSection)
+export default TrackingSection
