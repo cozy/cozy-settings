e2dc5d05007e764370791465b757fb6398319667
cballevre <celestin.ballevre@protonmail.com>
2023-09-14 18:58:23 +0200

    refactor(TrackingSection): Use cozy-client instead redux to manage state
    remove old

--- src/components/Profile/TrackingSection.jsx
+++ src/components/Profile/TrackingSection.jsx
@@ -1,11 +1,31 @@
 import React from 'react'
 
+import { useQuery, useMutation } from 'cozy-client'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import Input from 'components/Input'
+import { buildSettingsInstanceQuery } from 'lib/queries'
 
-export const TrackingSection = ({ instance, fields, onChange }) => {
+export const TrackingSection = () => {
   const { t } = useI18n()
+  const { mutate, mutationStatus } = useMutation()
+
+  const instanceQuery = buildSettingsInstanceQuery()
+  const { data: instance } = useQuery(
+    instanceQuery.definition,
+    instanceQuery.options
+  )
+
+  const handleChange = (_, value) => {
+    mutate({
+      _rev: instance.meta.rev,
+      ...instance,
+      attributes: {
+        ...instance.attributes,
+        tracking: value
+      }
+    })
+  }
 
   return (
     <Input
@@ -13,13 +33,15 @@ export const TrackingSection = ({ instance, fields, onChange }) => {
       type="checkbox"
       title={t('ProfileView.tracking.title')}
       label={t('ProfileView.tracking.label', {
-        version:
-          instance && instance.data.attributes.tos
-            ? `-${instance.data.attributes.tos}`
-            : '-201711'
+        version: instance?.tos ? `-${instance.tos}` : '-201711'
       })}
-      {...fields.tracking}
-      onChange={onChange}
+      value={Boolean(instance.tracking)}
+      onChange={handleChange}
+      submitting={mutationStatus === 'loading'}
+      saved={mutationStatus === 'loaded'}
+      errors={
+        mutationStatus === 'failed' ? ['ProfileView.infos.server_error'] : []
+      }
     />
   )
 }
