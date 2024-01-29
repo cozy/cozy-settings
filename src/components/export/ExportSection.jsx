e1c182e73a7a6d8216def250f83f91e659c1b912
cballevre <celestin.ballevre@protonmail.com>
2024-01-29 16:26:03 +0100

    refactor: Convert ExportSection in functional component

--- src/components/export/ExportSection.jsx
+++ src/components/export/ExportSection.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { STACK_DOMAIN } from 'actions'
 
-import { translate } from 'cozy-ui/transpiled/react/providers/I18n'
+import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 import { Button } from 'cozy-ui/transpiled/react/deprecated/Button'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 
@@ -14,116 +14,111 @@ import flag from 'cozy-flags'
 
 const exportImage = require('assets/images/export-cozy-mail.svg')
 
-class ExportSection extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      displayModal: false
-    }
-    this.toggleModal = this.toggleModal.bind(this)
-    this.submitExport = this.submitExport.bind(this)
+const ExportSection = ({
+  email,
+  exportData,
+  exportId,
+  fetchExportData,
+  parent,
+  requestExport
+}) => {
+  const { t } = useI18n()
+
+  const [displayModal, setDisplayModal] = useState(false)
+
+  const toggleModal = () => {
+    setDisplayModal(!displayModal)
   }
 
-  toggleModal() {
-    this.setState(currentState => ({
-      displayModal: !currentState.displayModal
-    }))
+  const submitExport = () => {
+    requestExport()
+    toggleModal()
   }
 
-  submitExport() {
-    this.props.requestExport()
-    this.toggleModal()
-  }
-
-  render() {
-    const { t, email, exportData, exportId, fetchExportData, parent } =
-      this.props
-    const { displayModal } = this.state
-    return (
+  return (
+    <div>
       <div>
-        <div>
-          <Typography variant="h5" gutterBottom>
-            {t('ProfileView.export.title')}
-          </Typography>
-          {flag('settings.moving-cozy') && (
-            <div className="u-mv-half">
-              <form
-                action={STACK_DOMAIN + '/move/initialize'}
-                method="post"
-                target="_blank"
-              >
-                <Button label={t('ProfileView.move.button')} extension="full" />
-              </form>
-            </div>
-          )}
-          <Typography variant="body1" gutterBottom>
-            {t('ProfileView.export.label')}
-          </Typography>
-          <div className="u-mb-half">
-            <Button
-              onClick={this.toggleModal}
-              label={t('ProfileView.export.link')}
-              extension="full"
-            />
+        <Typography variant="h5" gutterBottom>
+          {t('ProfileView.export.title')}
+        </Typography>
+        {flag('settings.moving-cozy') && (
+          <div className="u-mv-half">
+            <form
+              action={STACK_DOMAIN + '/move/initialize'}
+              method="post"
+              target="_blank"
+            >
+              <Button label={t('ProfileView.move.button')} extension="full" />
+            </form>
           </div>
+        )}
+        <Typography variant="body1" gutterBottom>
+          {t('ProfileView.export.label')}
+        </Typography>
+        <div className="u-mb-half">
+          <Button
+            onClick={toggleModal}
+            label={t('ProfileView.export.link')}
+            extension="full"
+          />
         </div>
-        {displayModal && (
-          <IllustrationDialog
-            open
-            onClose={() => this.toggleModal()}
-            title={
-              <div className="u-flex u-flex-column u-flex-items-center">
-                <img
-                  className="u-maw-4 u-mb-1"
-                  alt={t('ProfileView.export.modal.title')}
-                  src={exportImage}
-                />
-                {t('ProfileView.export.modal.title')}
-              </div>
-            }
-            content={
-              <>
-                <ReactMarkdownWrapper
-                  source={t('ProfileView.export.modal.description', { email })}
-                />
-                {exportData.error && (
-                  <Typography variant="body1" className="u-error">
-                    {t(exportData.error)}
-                  </Typography>
-                )}
-              </>
-            }
-            actions={
-              <>
-                <Button
-                  className="u-flex-grow-1"
-                  theme="secondary"
-                  onClick={this.toggleModal}
-                  label={t('ProfileView.export.modal.cancel')}
-                />
-                <Button
-                  className="u-flex-grow-1"
-                  theme="primary"
-                  onClick={this.submitExport}
-                  busy={exportData.submitting}
-                  disabled={exportData.submitting}
-                  label={t('ProfileView.export.modal.CTA')}
-                />
-              </>
-            }
-          />
-        )}
-        {exportId && (
-          <ExportDownload
-            exportData={exportData}
-            exportId={exportId}
-            fetchExportData={fetchExportData}
-            parent={parent}
-          />
-        )}
       </div>
-    )
-  }
+      {displayModal && (
+        <IllustrationDialog
+          open
+          onClose={toggleModal}
+          title={
+            <div className="u-flex u-flex-column u-flex-items-center">
+              <img
+                className="u-maw-4 u-mb-1"
+                alt={t('ProfileView.export.modal.title')}
+                src={exportImage}
+              />
+              {t('ProfileView.export.modal.title')}
+            </div>
+          }
+          content={
+            <>
+              <ReactMarkdownWrapper
+                source={t('ProfileView.export.modal.description', { email })}
+              />
+              {exportData.error && (
+                <Typography variant="body1" className="u-error">
+                  {t(exportData.error)}
+                </Typography>
+              )}
+            </>
+          }
+          actions={
+            <>
+              <Button
+                className="u-flex-grow-1"
+                theme="secondary"
+                onClick={toggleModal}
+                label={t('ProfileView.export.modal.cancel')}
+              />
+              <Button
+                className="u-flex-grow-1"
+                theme="primary"
+                onClick={submitExport}
+                busy={exportData.submitting}
+                disabled={exportData.submitting}
+                label={t('ProfileView.export.modal.CTA')}
+              />
+            </>
+          }
+        />
+      )}
+      {exportId && (
+        <ExportDownload
+          exportData={exportData}
+          exportId={exportId}
+          fetchExportData={fetchExportData}
+          parent={parent}
+        />
+      )}
+    </div>
+  )
 }
 
-export default translate()(ExportSection)
+export { ExportSection }
