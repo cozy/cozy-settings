e29cd8b8f4220f172750b01e959627f302c14e7e
cballevre <celestin.ballevre@protonmail.com>
2024-01-30 09:43:30 +0100

    test: Remove cozy-ui color mock
    We don't need to mock cozy-ui color because it manage test environnement now. This mock was also blocking test after the new version of cozy-ui 101.0.0

--- test/jestLib/setup.js
+++ test/jestLib/setup.js
@@ -6,15 +6,6 @@ process.env.USE_REACT = true
 
 window.__DEVELOPMENT__ = false
 
-// To avoid the errors while creating theme (since no CSS stylesheet
-// defining CSS variables is injected during tests)
-// Material-UI: the color provided to augmentColor(color) is invalid.
-// The color object needs to have a `main` property or a `500` property.
-jest.mock('cozy-ui/transpiled/react/utils/color', () => ({
-  getCssVariableValue: () => '#fff',
-  getInvertedCssVariableValue: () => '#fff'
-}))
-
 jest.mock('cozy-intent', () => ({
   WebviewIntentProvider: ({ children }) => children,
   useWebviewIntent: () => ({ call: () => {} })
