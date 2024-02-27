f5735ce6efdee39cbf89c90507d02f37d0973f6e
cballevre <celestin.ballevre@protonmail.com>
2024-02-27 16:27:22 +0100

    feat: Use premuim provider to enable link in flagship

--- src/components/Subscription/SubscriptionMenuItem.jsx
+++ src/components/Subscription/SubscriptionMenuItem.jsx
@@ -3,19 +3,18 @@ import React from 'react'
 import flag from 'cozy-flags'
 import CozyCircle from 'cozy-ui/transpiled/react/Icons/CozyCircle'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
-import { isFlagshipApp } from 'cozy-device-helper'
 
 import { MenuItemAnchor } from 'components/menu/MenuItemAnchor'
 import { MenuItemNavLink } from 'components/menu/MenuItemNavLink'
 import { routes } from 'constants/routes'
-import { useOffersLink } from 'hooks/useOffersLink'
+import { usePremium } from 'components/Premium/PremiumProvider'
 
 /**
  * Sidebar menu link for plan
  */
 const SubscriptionMenuItem = () => {
   const { t } = useI18n()
-  const offersLink = useOffersLink()
+  const { canOpenPremiumLink, premiumLink } = usePremium()
 
   if (flag('settings.subscription')) {
     return (
@@ -27,11 +26,11 @@ const SubscriptionMenuItem = () => {
     )
   }
 
-  if (offersLink && !isFlagshipApp()) {
+  if (canOpenPremiumLink) {
     return (
       <MenuItemAnchor
         primary={t('Nav.primary_plan')}
-        href={offersLink}
+        href={premiumLink}
         target="_blank"
         icon={CozyCircle}
       />
