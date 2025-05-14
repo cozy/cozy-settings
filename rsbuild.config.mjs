66b442faf89ee6e324863a965e14bf1d3de2615e
Théo Poizat <hello@zatteo.com>
2025-05-14 15:09:32 +0200

    feat: Update title

--- rsbuild.config.mjs
+++ rsbuild.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from '@rsbuild/core'
 import { getRsbuildConfig } from 'rsbuild-config-cozy-app'
 
 const config = getRsbuildConfig({
-  title: 'Cozy Settings',
+  title: 'Twake Settings',
   hasIntents: true
 })
 
