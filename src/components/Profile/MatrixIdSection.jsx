c1fb3143ff286688fdc448928be809c698905048
Théo Poizat <hello@zatteo.com>
2025-09-15 16:42:45 +0200

    feat: Display a generated Matrix ID

--- src/components/Profile/MatrixIdSection.jsx
+++ src/components/Profile/MatrixIdSection.jsx
@@ -1,20 +1,36 @@
 import React from 'react'
 
-import { useQuery } from 'cozy-client'
+import { useClient } from 'cozy-client'
 import Stack from 'cozy-ui/transpiled/react/Stack'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
 import Input from '@/components/Input'
-import { buildSettingsInstanceQuery } from '@/lib/queries'
+
+// FIXME: Currently, Matrix ID may not be generated correctly for some domains, like for nested domains.
+// We will later get more data to generate it correctly (in front end or back end) when the admin panel
+// project will be operational. In the meantime, we can hide this section with a flag when it is not displayed correctly.
+export const generateMatrixId = domain => {
+  if (!domain) {
+    return ''
+  }
+
+  const firstDotIndex = domain.indexOf('.')
+
+  if (firstDotIndex === -1) {
+    return ''
+  }
+
+  return `${domain.substring(0, firstDotIndex)}:${domain.substring(
+    firstDotIndex + 1
+  )}`
+}
 
 const MatrixIdSection = () => {
   const { t } = useI18n()
 
-  const instanceQuery = buildSettingsInstanceQuery()
-  const { data: instance } = useQuery(
-    instanceQuery.definition,
-    instanceQuery.options
-  )
+  const client = useClient()
+  const { domain } = client.getInstanceOptions()
+  const generatedMatrixId = generateMatrixId(domain)
 
   return (
     <Stack spacing="m">
@@ -23,9 +39,7 @@ const MatrixIdSection = () => {
         type="text"
         title={t('ProfileView.matrix_id.title')}
         label={t(`ProfileView.matrix_id.label`)}
-        value={
-          instance?.matrix_id || 'example:linagora.com (to be implemented)'
-        }
+        value={generatedMatrixId}
         copyable={true}
       />
     </Stack>
