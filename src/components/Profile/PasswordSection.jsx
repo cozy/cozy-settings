befaf44a3f8a6d106bc95ee46eba74ffb506c30d
Théo Poizat <hello@zatteo.com>
2025-09-16 09:40:09 +0200

    feat: Allow to change password if appropriate flag

--- src/components/Profile/PasswordSection.jsx
+++ src/components/Profile/PasswordSection.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
 
 import flag from 'cozy-flags'
 import Buttons from 'cozy-ui/transpiled/react/Buttons'
+import Stack from 'cozy-ui/transpiled/react/Stack'
 import Typography from 'cozy-ui/transpiled/react/Typography'
 import { useI18n } from 'cozy-ui/transpiled/react/providers/I18n'
 
@@ -12,21 +12,24 @@ const PasswordSection = () => {
   const { t } = useI18n()
   const { hasPassword } = useHasPassword()
 
+  const signUpUrl = flag('signup.url')
+  const isPasswordReadonly = flag('settings.password.readonly') || !signUpUrl
+
   return hasPassword ? (
-    <div>
+    <Stack spacing="m">
       <Typography variant="h5" gutterBottom>
         {t('ProfileView.password.title')}
       </Typography>
       <Typography variant="body1">{t('ProfileView.password.label')}</Typography>
       <Buttons
-        component={Link}
-        to="/profile/password"
-        label={t('ProfileView.password.cta')}
         variant="secondary"
-        className="u-mt-half u-mh-0"
-        disabled={flag('settings.password.readonly')}
+        size="medium"
+        label={t('ProfileView.password.cta')}
+        href={`${signUpUrl}/change-password`}
+        target="_blank"
+        disabled={isPasswordReadonly}
       />
-    </div>
+    </Stack>
   ) : null
 }
 
