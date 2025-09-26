250bc298dfa7233d1d5e08d46067d257b16d5f20
Théo Poizat <hello@zatteo.com>
2025-09-26 14:19:18 +0200

    Revert "fix: Do not need a password to show password update button"
    This reverts commit df4ce45f4fe2dfd599f4906bbc5a404e7f89438b.

--- src/components/Profile/PasswordSection.spec.jsx
+++ src/components/Profile/PasswordSection.spec.jsx
@@ -1,24 +1,17 @@
 import { screen, render } from '@testing-library/react'
 import React from 'react'
 
-import flag from 'cozy-flags'
-
 import PasswordSection from '@/components/Profile/PasswordSection'
+import { useHasPassword } from '@/hooks/useHasPassword'
 import AppLike from '@/test/AppLike'
 
-jest.mock('cozy-flags')
+jest.mock('@/hooks/useHasPassword', () => ({
+  useHasPassword: jest.fn()
+}))
 
 describe('PasswordSection', () => {
-  const setup = ({
-    signUpUrl = 'https://signup.example.com',
-    isPasswordReadonly = false
-  }) => {
-    flag.mockImplementation(key => {
-      if (key === 'signup.url') return signUpUrl
-      if (key === 'settings.password.readonly') return isPasswordReadonly
-      return undefined
-    })
-
+  const setup = ({ hasPassword }) => {
+    useHasPassword.mockReturnValue({ hasPassword })
     render(
       <AppLike>
         <PasswordSection />
@@ -26,27 +19,13 @@ describe('PasswordSection', () => {
     )
   }
 
-  afterEach(() => {
-    jest.clearAllMocks()
+  it('should render the button by default', () => {
+    setup({ hasPassword: true })
+    expect(screen.getByText('Update my password')).toBeTruthy()
   })
 
-  it('should render the button when signUpUrl is available and password is not readonly', () => {
-    setup({
-      signUpUrl: 'https://signup.example.com',
-      isPasswordReadonly: false
-    })
-    const button = screen.getByRole('link')
-    expect(button).toBeTruthy()
-    expect(button).not.toHaveAttribute('aria-disabled', 'true')
-  })
-
-  it('should disable the button when password is readonly', () => {
-    setup({ signUpUrl: 'https://signup.example.com', isPasswordReadonly: true })
-    expect(screen.getByRole('link')).toHaveAttribute('aria-disabled', 'true')
-  })
-
-  it('should disable the button when signUpUrl is not available', () => {
-    setup({ signUpUrl: null, isPasswordReadonly: false })
-    expect(screen.getByRole('link')).toHaveAttribute('aria-disabled', 'true')
+  it('should hide the button if user doesnt have a password', () => {
+    setup({ hasPassword: false })
+    expect(screen.queryByText('Update my password')).toBeFalsy()
   })
 })
