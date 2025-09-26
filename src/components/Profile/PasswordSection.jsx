51459b16829f4320f8bef36105f31b4f3dc375a0
Théo Poizat <hello@zatteo.com>
2025-09-26 14:35:42 +0200

    feat: Change how PasswordSection is displayed
    If we have a signup URL, we redirect to this URL.
    
    Else if we have a password, we redirect to good old Cozy change
    password page.
    
    Else we render nothing.
    
    In both case, if flag settings.password.readonly is true, we disable
    to button to have full control.

--- src/components/Profile/PasswordSection.jsx
+++ src/components/Profile/PasswordSection.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 import flag from 'cozy-flags'
 import Buttons from 'cozy-ui/transpiled/react/Buttons'
@@ -13,24 +14,51 @@ const PasswordSection = () => {
   const { hasPassword } = useHasPassword()
 
   const signUpUrl = flag('signup.url')
-  const isPasswordReadonly = flag('settings.password.readonly') || !signUpUrl
+  const isPasswordReadonly = flag('settings.password.readonly')
 
-  return hasPassword ? (
-    <Stack spacing="m">
-      <Typography variant="h5" gutterBottom>
-        {t('ProfileView.password.title')}
-      </Typography>
-      <Typography variant="body1">{t('ProfileView.password.label')}</Typography>
-      <Buttons
-        variant="secondary"
-        size="medium"
-        label={t('ProfileView.password.cta')}
-        href={`${signUpUrl}/change-password`}
-        target="_blank"
-        disabled={isPasswordReadonly}
-      />
-    </Stack>
-  ) : null
+  if (signUpUrl) {
+    return (
+      <Stack spacing="m">
+        <Typography variant="h5" gutterBottom>
+          {t('ProfileView.password.title')}
+        </Typography>
+        <Typography variant="body1">
+          {t('ProfileView.password.label')}
+        </Typography>
+        <Buttons
+          variant="secondary"
+          size="medium"
+          label={t('ProfileView.password.cta')}
+          href={`${signUpUrl}/change-password`}
+          target="_blank"
+          disabled={isPasswordReadonly}
+        />
+      </Stack>
+    )
+  }
+
+  if (hasPassword) {
+    return (
+      <Stack spacing="m">
+        <Typography variant="h5" gutterBottom>
+          {t('ProfileView.password.title')}
+        </Typography>
+        <Typography variant="body1">
+          {t('ProfileView.password.label')}
+        </Typography>
+        <Buttons
+          variant="secondary"
+          size="medium"
+          label={t('ProfileView.password.cta')}
+          component={Link}
+          to="/profile/password"
+          disabled={isPasswordReadonly}
+        />
+      </Stack>
+    )
+  }
+
+  return null
 }
 
 export default PasswordSection
