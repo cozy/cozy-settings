f1acf42404b91fdfd6c711411e1f1fc1bf4fd430
JF-Cozy <julien.ficheux@cozycloud.cc>
2025-10-15 15:27:22 +0200

    refactor(AvatarSection): Move input in Menu to have all in the same place
    all element related to upload / delete avatar are in the same place

--- src/components/Profile/AvatarSection/index.jsx
+++ src/components/Profile/AvatarSection/index.jsx
@@ -1,91 +1,57 @@
 import React, { useState, useRef } from 'react'
 
-import { useClient } from 'cozy-client'
 import Badge from 'cozy-ui/transpiled/react/Badge'
 import Button from 'cozy-ui/transpiled/react/Buttons'
-import { useI18n } from 'cozy-ui/transpiled/react/I18n'
 import Icon from 'cozy-ui/transpiled/react/Icon'
 import PenIcon from 'cozy-ui/transpiled/react/Icons/Pen'
-import { useAlert } from 'cozy-ui/transpiled/react/providers/Alert'
 
-import { useAvatar } from './AvatarContext'
 import AvatarMenu from './AvatarMenu'
 import AvatarWrapper from './AvatarWrapper'
-import { handleFileChange } from './helpers'
 
 const AvatarSection = () => {
   const [showMenu, setShowMenu] = useState(false)
   const [avatarStatus, setAvatarStatus] = useState('PRESENT') // PRESENT || ABSENT || LOADING
   const [avatarTimestamp, setAvatarTimestamp] = useState(Date.now())
-  const client = useClient()
-  const { t } = useI18n()
-  const { showAlert } = useAlert()
-  const { uploadAvatar } = useAvatar()
 
-  const fileInputRef = useRef(null)
   const menuAnchorRef = useRef(null)
 
   return (
-    <>
-      <input
-        type="file"
-        ref={fileInputRef}
-        style={{ display: 'none' }}
-        accept="image/*"
-        onChange={event =>
-          handleFileChange({
-            event,
-            client,
-            t,
-            fileInputRef,
-            avatarStatus,
-            uploadAvatar,
-            setAvatarStatus,
-            setAvatarTimestamp,
-            setShowMenu,
-            showAlert
-          })
-        }
+    <Badge
+      anchorOrigin={{
+        vertical: 'bottom',
+        horizontal: 'right'
+      }}
+      badgeContent={
+        <>
+          <Button
+            ref={menuAnchorRef}
+            component="div"
+            className="u-miw-auto u-w-2-half u-h-2-half u-bdrs-circle"
+            classes={{ label: 'u-flex u-w-auto' }}
+            style={{
+              outline: '4px solid var(--paperBackgroundColor)'
+            }}
+            label={<Icon icon={PenIcon} />}
+            size="small"
+            onClick={() => setShowMenu(v => !v)}
+          />
+          <AvatarMenu
+            anchorRef={menuAnchorRef.current}
+            avatarStatus={avatarStatus}
+            showMenu={showMenu}
+            setShowMenu={setShowMenu}
+            setAvatarStatus={setAvatarStatus}
+            setAvatarTimestamp={setAvatarTimestamp}
+          />
+        </>
+      }
+    >
+      <AvatarWrapper
+        avatarStatus={avatarStatus}
+        setAvatarStatus={setAvatarStatus}
+        avatarTimestamp={avatarTimestamp}
       />
-      <Badge
-        anchorOrigin={{
-          vertical: 'bottom',
-          horizontal: 'right'
-        }}
-        badgeContent={
-          <>
-            <Button
-              ref={menuAnchorRef}
-              component="div"
-              className="u-miw-auto u-w-2-half u-h-2-half u-bdrs-circle"
-              classes={{ label: 'u-flex u-w-auto' }}
-              style={{
-                outline: '4px solid var(--paperBackgroundColor)'
-              }}
-              label={<Icon icon={PenIcon} />}
-              size="small"
-              onClick={() => setShowMenu(v => !v)}
-            />
-            {showMenu && (
-              <AvatarMenu
-                fileInputRef={fileInputRef}
-                anchorRef={menuAnchorRef.current}
-                avatarStatus={avatarStatus}
-                setShowMenu={setShowMenu}
-                setAvatarStatus={setAvatarStatus}
-                setAvatarTimestamp={setAvatarTimestamp}
-              />
-            )}
-          </>
-        }
-      >
-        <AvatarWrapper
-          avatarStatus={avatarStatus}
-          setAvatarStatus={setAvatarStatus}
-          avatarTimestamp={avatarTimestamp}
-        />
-      </Badge>
-    </>
+    </Badge>
   )
 }
 
